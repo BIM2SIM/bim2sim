924cd4e2b69e9138aa5d81abd5368268ea449817
David Jansen <david.jansen@eonerc.rwth-aachen.de>
2018-11-21 11:40:38 +0100

    testfile for plantsystem

---
+++ MainLib/test/testplantsystem.py
@@ -0,0 +1,21 @@
+import unittest
+
+class Test_Plantsystem(unittest.TestCase):
+    def setUp(self):
+        """
+        setup function for test
+        :return:
+        """
+        pass
+    def tearDown(self):
+        """
+        teardown function for test
+        :return:
+        """
+        pass
+
+    def test_myfirsttest(self):
+        self.fail("Not implemented yet")
+
+if __name__ == '__main__':
+    unittest.main()
