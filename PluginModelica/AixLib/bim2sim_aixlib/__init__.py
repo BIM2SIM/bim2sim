d0c29211c31a0dd05c1eb0f4fe699a90db0bd37f
David Jansen <david.jansen@eonerc.rwth-aachen.de>
2018-11-21 11:10:14 +0100

    add demo files for aixlib plugin

---
+++ PluginModelica/AixLib/bim2sim_aixlib/__init__.py
@@ -0,0 +1,8 @@
+﻿'''EnergyPlus plugin for bim2sim
+
+Holds logic to run a simulation based on prepared ifc data
+'''
+
+from .aixlib import EnergyPlus
+
+contend = {'energyplus':EnergyPlus}
---
+++ PluginModelica/AixLib/bim2sim_aixlib/aixlib.py
@@ -0,0 +1,21 @@
+import bim2sim
+
+class AixLib(bim2sim.SimulationBase):
+
+	def __init__(self):
+		super().__init__()
+		
+		# do stuff
+		return
+
+	def prepare(self, model):
+		
+		self.logger.info('preparing stuff')
+
+		return
+
+	def run(self):
+
+		self.logger.info('doing stuff')
+
+		return
