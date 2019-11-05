3dea8ba2c24a55d218a277cd53bc7b55e9e6efda
Eric Fichter <fichter@e3d.rwth-aachen.de>
2019-11-05 16:03:27 +0100

    added cfd stuff

---
+++ PluginCFD/bim2sim_cfd/cfd.py
@@ -0,0 +1,20 @@
+﻿from bim2sim.manage import BIM2SIMManager
+from bim2sim.project import PROJECT
+from bim2sim.workflow.cfd import Facade
+
+
+
+class CFDManager(BIM2SIMManager):
+
+    def __init__(self, task):
+        super().__init__(task)
+
+        self.facade = Facade()
+
+    def run(self):
+        print("CFD started")
+        path = PROJECT.ifc # path to ifc file
+        self.facade.run(path)
+
+
+
