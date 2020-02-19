ca8d3cc0881fdeef57ed4210a3e1ff44d8e5ea2a
CWarnecke <cwarnecke@rom-technik.de>
2020-02-19 14:33:35 +0100

    removed obsolete tests

--- MainLib/test/kernel/test_element.py
+++ MainLib/test/kernel/test_element.py
@@ -1,126 +1,41 @@
-﻿"""Testing Elements using Finder
-
-Extracting a property from an ifc class starts the way in 'ment to by' by using the properties implemented in Element classes.
-if a not None value is returned -> done
-None is retuned -> Finder should provide result (None on Finder AttributeError)
-AttributeError is raised -> Finder should provide result
-other Exception is raised -> raise
-
-Finder can return not None value -> done
-return None -> done (no value found)
-raise AttributeError -> attribute does not exist on finder
-raise other Exeption -> raise"""
+﻿"""Testing classes of module element"""
 
 import unittest
-import os
 
-import bim2sim
-from bim2sim import kernel
-from bim2sim.kernel import finder, element, elements
+from bim2sim.kernel import element
+from bim2sim.kernel.attribute import Attribute
 
-IFC_PATH = os.path.abspath(os.path.join(
-    os.path.dirname(bim2sim.__file__), '../..', 
-    r'ExampleFiles/KM_DPM_Vereinshaus_Gruppe62_Heizung_DTV_all_elements.ifc'))
+from test.kernel.helper import SetupHelper
 
-FINDER_RESULT = 'foo'
 
-class DummyFinder(finder.Finder):
+# IFC_PATH = os.path.abspath(os.path.join(
+#     os.path.dirname(bim2sim.__file__), '../..',
+#     r'ExampleFiles/KM_DPM_Vereinshaus_Gruppe62_Heizung_DTV_all_elements.ifc'))
 
-    def find(self, element, property_name:str):
-        if property_name.startswith('prop'):
-            if property_name.endswith('findernone'):
-                return None
-            return FINDER_RESULT
-        else:
-            raise AttributeError
 
-class DummyElement(element.Element):
+class TestElement(element.Element):
     ifc_type = "IfcPipeFitting"
 
-    @property
-    def prop_ok(self):
-        return 12.3
+    attr1 = Attribute(
+        name='attr1'
+    )
 
-    @property
-    def prop_none(self):
-        return None
+    attr2 = Attribute(
+        name='attr2'
+    )
 
-    @property
-    def prop_findernone(self):
-        return None
+    attr3 = Attribute(
+        name='attr3'
+    )
 
-    @property
-    def prop_raise_attributerror(self):
-        raise AttributeError
+    attr4 = Attribute(
+        name='attr4'
+    )
 
-    @property
-    def prop_raises_arithmeticerror(self):
-        raise ArithmeticError
 
-    @property
-    def nofind_prop_none(self):
-        return None
-
-    @property
-    def nofind_prop_raise_attributerror(self):
-        raise AttributeError
-
-    @property
-    def nofind_prop_raises_arithmeticerror(self):
-        raise ArithmeticError
-
-@unittest.skip("Finder changed. Test needs update")  # TODO!
-class Test_ElementProperties(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.ifc = ifc2python.load_ifc(IFC_PATH)
-        cls.backup = element.Element.finder
-        element.Element.finder = DummyFinder()
-
-    @classmethod
-    def tearDownClass(cls):
-        element.Element.finder = cls.backup
-
-    def setUp(self):
-        ifc = self.__class__.ifc.by_type("IfcPipeFitting")[0]
-        self.ele = DummyElement(ifc=ifc)
-
-    def test_ok(self):
-        """default returns value"""
-        self.assertEqual(self.ele.prop_ok, 12.3)
-
-    def test_none(self):
-        """no default result from element but found by finder"""
-        self.assertEqual(self.ele.prop_none, FINDER_RESULT)
-        self.assertIsNone(self.ele.prop_findernone)
-
-    def test_invalid(self):
-        """getting an invalid attribut will raise"""
-        with self.assertRaises(AttributeError):
-            self.ele.invalid_attribute
-
-    def test_only_known_by_finder(self):
-        """finder can provide results for not default properties"""
-        self.assertEqual(self.ele.prop_xyz, FINDER_RESULT)
-
-    def test_prop_raises_attributeerror(self):
-        """property raises AttributeError but finder returns result"""
-        self.assertEqual(self.ele.prop_raise_attributerror, FINDER_RESULT)
-
-    def test_prop_raises_error(self):
-        """property raises != AttributeError will raise"""
-        with self.assertRaises(ArithmeticError):
-            self.ele.prop_raises_arithmeticerror
-
-    def test_nofind_prop_raises(self):
-        """property raises and finder returns None"""
-
-        with self.assertRaises(AttributeError):
-            self.ele.nofind_prop_raise_attributerror
-
-        with self.assertRaises(ArithmeticError):
-            self.ele.nofind_prop_raises_arithmeticerror
+@unittest.skip("To be defined")  # TODO: test factory, get_by_guid, discard, Dummy, Ports, connections
+class TestElement(unittest.TestCase):
+    pass
 
 
 if __name__ == '__main__':
