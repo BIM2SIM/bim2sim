2275dc52c715d799646945c969c6132bf41444da
DaJansenGit <david.jansen1@rwth-aachen.de>
2020-06-02 14:32:00 +0200

    make path for test abs also

--- MainLib/run_tests.py
+++ MainLib/run_tests.py
@@ -1,9 +1,11 @@
 ﻿"""Run all tests"""
+from os import path
+this_dir = path.abspath(path.dirname(__file__))
 
 if __name__ == '__main__':
     import unittest
 
     loader = unittest.TestLoader()
-    tests = loader.discover('.')
+    tests = loader.discover(path.join(this_dir))
     testRunner = unittest.runner.TextTestRunner()
     testRunner.run(tests)
