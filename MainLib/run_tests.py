5686edddd88e6820eb3635986dbb97eb889c942b
DaJansenGit <david.jansen1@rwth-aachen.de>
2020-06-02 13:55:41 +0200

    change discover dir

--- MainLib/run_tests.py
+++ MainLib/run_tests.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     import unittest
 
     loader = unittest.TestLoader()
-    tests = loader.discover('./test')
+    tests = loader.discover('.')
     testRunner = unittest.runner.TextTestRunner()
     testRunner.run(tests)
