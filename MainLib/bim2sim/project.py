ca733286f8759bb2a7ef69b2317e9aa1390eec87
CWarnecke <cwarnecke@rom-technik.de>
2020-07-16 14:09:21 +0200

    remove cwd change

--- MainLib/bim2sim/project.py
+++ MainLib/bim2sim/project.py
@@ -73,8 +73,6 @@ class _Project():
     def root(self, value):
         if value:
             self._rootpath = Path(value).absolute().resolve()
-            print("Changing working directory to '%s'", self._rootpath)
-            os.chdir(self._rootpath)
         else:
             self._rootpath = None
         print("Rootpath set to '%s'"%(value))
