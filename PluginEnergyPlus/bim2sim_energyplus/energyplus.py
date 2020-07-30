80c7a4461578e81b81cc3a1e68d119b10bf7c95b
veronika_richter <richter@e3d.rwth-aachen.de>
2020-07-30 16:10:57 +0200

    Initialize energyplus.py

--- PluginEnergyPlus/bim2sim_energyplus/energyplus.py
+++ PluginEnergyPlus/bim2sim_energyplus/energyplus.py
@@ -1,9 +1,13 @@
 ﻿
 
-import bim2sim
-from bim2sim.manage import BIM2SIMManager
+from bim2sim.manage import BIM2SIMManager, PROJECT
+from bim2sim.task import bps, base, common, hvac
+from bim2sim.task.sub_tasks import tz_detection
 
 class EnergyPlus(BIM2SIMManager):
+
+    def __init__(self, workflow):
+        super().__init__(workflow)
 
     def prepare(self, model):
         
@@ -13,6 +17,19 @@ class EnergyPlus(BIM2SIMManager):
 
     def run(self):
 
-        self.logger.info('doing stuff')
+        self.playground.run_task(bps.SetIFCTypesBPS())
+        self.playground.run_task(common.LoadIFC())
+        self.playground.run_task(bps.Inspect())
+
+        sb_only = bps.Inspect.filter_instances(self.playground.state['instances'], "SpaceBoundary")
+
+        for bound in sb_only:
+            print(bound.guid, bound.bound_instance, bound.position)
+
+        print(self.playground.state.keys())
+        print(sb_only)
+
+
+        pass
 
         return
