fcf6422d14fdfee82f9781b7b8ee17b2c78de93c
Eric Fichter <fichter@e3d.rwth-aachen.de>
2020-09-16 14:43:48 +0200

    Replace __init__.py

--- MainLib/bim2sim/__init__.py
+++ MainLib/bim2sim/__init__.py
@@ -11,7 +11,7 @@ import pkg_resources
 from bim2sim.kernel import ifc2python
 from bim2sim.manage import BIM2SIMManager
 from bim2sim.project import PROJECT, get_config
-from bim2sim.workflow import PlantSimulation
+from bim2sim.workflow import PlantSimulation, BPSMultiZoneSeparated
 
 VERSION = '0.1-dev'
 
@@ -41,11 +41,11 @@ def get_backends(by_entrypoint=False):
     return sim
 
 
-
 def finish():
     """cleanup method"""
     logger = logging.getLogger(__name__)
     logger.info('finished')
+
 
 def logging_setup():
     """Setup for logging module"""
@@ -77,7 +77,7 @@ def main(rootpath=None):
     _rootpath = rootpath or os.getcwd()
     PROJECT.root = _rootpath
     assert PROJECT.is_project_folder(), \
-        "'%s' does not look like a project folder. Create a project folder first."%(_rootpath)
+        "'%s' does not look like a project folder. Create a project folder first." % (_rootpath)
 
     logging_setup()
     logger = logging.getLogger(__name__)
@@ -89,24 +89,26 @@ def main(rootpath=None):
     assert backend, "No backend set. Check config.ini"
 
     logger.info("Loading backend '%s' ...", backend)
+    print(plugins)
     manager_cls = plugins.get(backend.lower())()
 
     if manager_cls is None:
-        msg = "Simulation '%s' not found in plugins. Available plugins:\n - "%(backend)
+        msg = "Simulation '%s' not found in plugins. Available plugins:\n - " % (backend)
         msg += '\n - '.join(list(plugins.keys()) or ['None'])
         raise AttributeError(msg)
 
     if not BIM2SIMManager in manager_cls.__bases__:
-        raise AttributeError("Got invalid manager from %s"%(backend))
+        raise AttributeError("Got invalid manager from %s" % (backend))
 
-    workflow = PlantSimulation() #TODO
+    # workflow = PlantSimulation()  # TODO
+    workflow = BPSMultiZoneSeparated()  # TODO
 
     # prepare simulation
     manager = manager_cls(workflow)
 
     # run Manager
     manager.run()
-    #manager.run_interactive()
+    # manager.run_interactive()
 
     finish()
 
@@ -114,12 +116,12 @@ def main(rootpath=None):
 def _debug_run_hvac():
     """Create example project and copy ifc if necessary"""
     path_base = os.path.abspath(os.path.join(os.path.dirname(__file__), "..\\.."))
-    rel_example = 'ExampleFiles/KM_DPM_Vereinshaus_Gruppe62_Heizung_DTV_all_elements.ifc'
+    rel_example = 'ExampleFiles/KM_DPM_Vereinshaus_Gruppe62_Heizung_with_pumps.ifc'
     path_ifc = os.path.normpath(os.path.join(path_base, rel_example))
     path_example = r"C:\temp\bim2sim\testproject"
 
     if not PROJECT.is_project_folder(path_example):
-        PROJECT.create(path_example, path_ifc, 'hkesim',)
+        PROJECT.create(path_example, path_ifc, 'hkesim', )
 
     main(path_example)
 
@@ -128,16 +130,31 @@ def _debug_run_bps():
     """Create example project and copy ifc if necessary"""
     path_base = os.path.abspath(os.path.join(os.path.dirname(__file__), "..\\.."))
 
-    rel_example = 'ExampleFiles/KM_DPM_Vereinshaus_Gruppe62_Architektur' \
-                      '.ifc'
+    rel_example = 'ExampleFiles/AC20-FZK-Haus.ifc'
+    # rel_example = 'ExampleFiles/KM_DPM_Vereinshaus_Gruppe62_Architektur_spaces.ifc'
     path_ifc = os.path.normpath(os.path.join(path_base, rel_example))
-    path_example = r"C:\temp\bim2sim\testproject_bps"
+    path_example = r"C:\temp\bim2sim\testproject_bps2"
 
     if not PROJECT.is_project_folder(path_example):
         PROJECT.create(path_example, path_ifc, 'TEASER')
 
     main(path_example)
 
+
+def _debug_run_cfd():
+    """Create example project and copy ifc if necessary"""
+
+    sys.path.append("/home/fluid/Schreibtisch/B/bim2sim-coding/PluginCFD")
+    path_example = r"/home/fluid/Schreibtisch/B/temp"
+    # unter ifc muss datei liegen
+
+    if not PROJECT.is_project_folder(path_example):
+        PROJECT.create(path_example, target='cfd')
+
+    main(path_example)
+
+
 if __name__ == '__main__':
+    #_debug_run_cfd()
+    _debug_run_bps()
     _debug_run_hvac()
-    # _debug_run_bps()
