b4b21468d32b9c26f0563a7e93452f571ebd689f
DaJansenGit <david.jansen1@rwth-aachen.de>
2020-10-02 15:28:17 +0200

    start module export models aixlib

--- PluginAixLib/bim2sim_aixlib/models/__init__.py
+++ PluginAixLib/bim2sim_aixlib/models/__init__.py
@@ -2,25 +2,45 @@
 
 from bim2sim.export import modelica
 from bim2sim.kernel import elements
-from bim2sim.kernel.aggregation import PipeStrand
+from bim2sim.kernel.aggregation import PipeStrand, Consumer, \
+    ConsumerHeatingDistributorModule, Generator_One_Fluid
 
-from bim2sim.export.modelica import standardlibrary # impor necessary for model detection
 
 class AixLib(modelica.Instance):
     library = "AixLib"
 
 
+# copy paste from hkesim
 class Boiler(AixLib):
     path = "AixLib.FastHVAC.Components.HeatGenerators.Boiler.Boiler"
-    represents = elements.Boiler
+    represents = [elements.Boiler]
 
     def __init__(self, element):
         self.check_power = self.check_numeric(min_value=0) #TODO: Checking System
         super().__init__(element)
 
     def get_params(self):
-        self.manage_param("nominal_power", self.element.rated_power,
-                          self.check_power)
+        self.register_param("rated_power", self.check_power, "nominal_power")
+
+
+class BoilerModule(AixLib):
+    path = "ModularEnergySystems.EnergyModules.BoilerSystem"
+    represents = [Generator_One_Fluid]
+
+    def __init__(self, element):
+        self.check_temp_tupel = True #TODO: Checking System
+        super().__init__(element)
+
+    def get_params(self):
+        # self.register_param("Tconsumer", self.check_temp_tupel, "Tconsumer")
+        self.params["Q_nom"] =
+        self.params["Tconsumer"] = (self.element.temperature_inlet, self.element.temperature_outlet)
+        self.params["Medium_heating"] = 'Modelica.Media.Water.ConstantPropertyLiquidWater'
+        self.register_param("useHydraulicSeparator", self.check_temp_tupel, "useHydraulicSeparator")
+        self.register_param("hydraulicSeparatorVolume", self.check_temp_tupel, "V")
+
+        index = 0
+
 
 
 
