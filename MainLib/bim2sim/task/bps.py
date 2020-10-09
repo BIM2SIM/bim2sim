7a96bc976812d6d45c013fbc9533dff5e765710a
veronika_richter <richter@e3d.rwth-aachen.de>
2020-10-09 16:13:12 +0200

    Modify minimum area for 2B bounds

--- MainLib/bim2sim/task/bps.py
+++ MainLib/bim2sim/task/bps.py
@@ -1887,7 +1887,7 @@ class ExportEP(ITask):
         for i, face in enumerate(faces):
             b_bound = SpaceBoundary2B()
             b_bound.bound_shape = face
-            if b_bound.bound_area < 0.1:
+            if b_bound.bound_area <1e-6:
                 continue
             b_bound.guid = space_obj.ifc.GlobalId + "_2B_" + str("%003.f"%(i+1))
             b_bound.thermal_zones.append(space_obj)
