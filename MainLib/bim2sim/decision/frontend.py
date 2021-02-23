b1d40883a280c91b4b1c9d87c3c088a2e1a66a18
Christian <cwarnecke@rom-technik.de>
2021-02-23 16:49:47 +0100

    remove unused method

--- MainLib/bim2sim/decision/frontend.py
+++ MainLib/bim2sim/decision/frontend.py
@@ -18,9 +18,6 @@ class FrontEnd:
     def get_question(self, decision):
         return decision.get_question()
 
-    def get_default(self, decision):
-        return decision.default
-
     def get_body(self, decision):
         return decision.get_body()
 
