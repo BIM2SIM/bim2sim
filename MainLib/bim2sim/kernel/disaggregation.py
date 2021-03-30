units problem corrected on BindThermalZones.py, structure of ThermalZones improved.

MainLib/bim2sim/kernel/disaggregation.py
@@ -66,12 +66,13 @@ class Disaggregation(BaseElement):
         i = len(parent.sub_instances)
         new_pos = np.array(space_boundary.position)
         area_disaggregation = space_boundary.bound_area
-        if hasattr(parent, 'gross_side_area'):
-            parent_area = parent.gross_side_area
+        if hasattr(parent, 'gross_area'):
+            parent_area = parent.gross_area
         else:
             parent_area = parent.area
 
-        if abs(parent_area - area_disaggregation) < 0.1 or area_disaggregation == 0 or len(parent.space_boundaries) == 1:
+        if abs(parent_area.m - area_disaggregation.m) < 0.1 or area_disaggregation.m == 0 or \
+                len(parent.space_boundaries) == 1:
             return parent
 
         else: