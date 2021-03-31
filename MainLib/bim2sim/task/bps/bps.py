71fa9c9b5b4dab7e2728dfff19e6bfacc50c872c
Veronika Richter <veronika.richter@rwth-aachen.de>
2021-03-31 20:26:56 +0200

    bug fix for missing long names (roomkeys) for energyplus

--- MainLib/bim2sim/task/bps/bps.py
+++ MainLib/bim2sim/task/bps/bps.py
@@ -1871,8 +1871,10 @@ class ExportEP(ITask):
         uc_path = PROJECT.root / 'MaterialTemplates/UseConditions.json'
         with open(uc_path) as json_file:
             uc_file = json.load(json_file)
-        room_key = [v for k, v in zone_dict.items() if k in key]
-        if room_key == []:
+        room_key = []
+        if key is not None:
+            room_key = [v for k, v in zone_dict.items() if k in key]
+        if not room_key:
             room_key = ['Single office']
         room = dict([k for k in uc_file.items() if type(k[1]) == dict])[room_key[0]]
         return room, room_key
