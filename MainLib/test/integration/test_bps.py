c599401eb546d2f0add780e1d6a627e8a6b051ec
Diego Cordoba <diego.cordoba@eonerc.rwth-aachen.de>
2021-04-09 14:46:21 +0200

    renamed tests

--- MainLib/test/integration/test_bps.py
+++ MainLib/test/integration/test_bps.py
@@ -13,46 +13,41 @@ from .base import IntegrationBase
 
 class TestIntegrationTEASER(IntegrationBase, unittest.TestCase):
 
-    def test_run_kitfzkhaus_test1(self):
+    def test_run_kitfzkhaus_spaces_low_layers_low(self):
         """Run project with AC20-FZK-Haus.ifc"""
         ifc = 'AC20-FZK-Haus.ifc'
-        # test -> spaces-low, layer-low
         answers = (True, True, 'Living', 'heavy', 'EnEv')
         with bim2sim.decision.Decision.debug_answer(answers, multi=True):
             return_code = self.run_project(ifc, 'TEASER')
         self.assertEqual(0, return_code, "Project did not finish successfully.")
 
-    def test_run_kitoffice_test1(self):
+    def test_run_kitoffice_spaces_low_layers_low(self):
         """Run project with AC20-Institute-Var-2.ifc"""
         ifc = 'AC20-Institute-Var-2.ifc'
-        # test -> spaces-low, layer-low
         answers = (True, True, 'Open-plan Office (7 or more employees)', 2015, 'heavy', 'EnEv')
         with bim2sim.decision.Decision.debug_answer(answers, multi=True):
             return_code = self.run_project(ifc, 'TEASER')
         self.assertEqual(0, return_code, "Project did not finish successfully.")
 
-    # def test_run_kitfzkhaus_test2(self):
+    # def test_run_kitfzkhaus_spaces_medium_layers_low(self):
     #     """Run project with AC20-FZK-Haus.ifc"""
     #     ifc = 'AC20-FZK-Haus.ifc'
-    #     # test -> spaces-medium, layer-low
     #     answers = (True, True, 'Kitchen - preparations, storage', 'heavy', 'EnEv', False)
     #     with bim2sim.decision.Decision.debug_answer(answers, multi=True):
     #         return_code = self.run_project(ifc, 'TEASER')
     #     self.assertEqual(0, return_code, "Project did not finish successfully.")
 
-    # def test_run_kitoffice_test2(self):
+    # def test_run_kitoffice_spaces_medium_layers_low(self):
     #     """Run project with AC20-Institute-Var-2.ifc"""
     #     ifc = 'AC20-Institute-Var-2.ifc'
-    #     # test -> spaces-medium, layer-low
     #     answers = (True, True, 2015, 'heavy', 'EnEv', False)
     #     with bim2sim.decision.Decision.debug_answer(answers, multi=True):
     #         return_code = self.run_project(ifc, 'TEASER')
     #     self.assertEqual(0, return_code, "Project did not finish successfully.")
 
-    # def test_run_kitfzkhaus_test3(self):
+    # def test_run_kitfzkhaus_spaces_medium_layers_full(self):
     #     """Run project with AC20-FZK-Haus.ifc"""
     #     ifc = 'AC20-FZK-Haus.ifc'
-    #     # test -> spaces-medium, layer-full
     #     answers = (True, True, 'Kitchen - preparations, storage', True, 'Concrete_DK', True, 'hardwood', True,
     #                'Light_Concrete_DK', True, 'solid_brick_a', 'heavy', 1, 'Brick', 'brick_H', 'EnEv', 1, 'Glas',
     #                'glas_generic', False)
@@ -62,10 +57,9 @@ class TestIntegrationTEASER(IntegrationBase, unittest.TestCase):
     #         return_code = self.run_project(ifc, 'TEASER')
     #     self.assertEqual(0, return_code, "Project did not finish successfully.")
 
-    # def test_run_kitoffice_test3(self):
+    # def test_run_kitoffice_spaces_medium_layers_full(self):
     #     """Run project with AC20-Institute-Var-2.ifc"""
     #     ifc = 'AC20-Institute-Var-2.ifc'
-    #     # test -> spaces-medium, layer-full
     #     answers = (True, True, True, 'belgian_brick', 'Glas', True, 'glas_generic', 500, 1.5, 0.2, True, 'air_layer',
     #                0.1, True, 'Concrete_DK', 2015, 'heavy', 1, 'Beton', 'Light_Concrete_DK', 1, 'Beton', 1, 'Door', 1,
     #                'Beton', False)
@@ -73,10 +67,9 @@ class TestIntegrationTEASER(IntegrationBase, unittest.TestCase):
     #         return_code = self.run_project(ifc, 'TEASER')
     #     self.assertEqual(0, return_code, "Project did not finish successfully.")
 
-    # def test_run_kitfzkhaus_test4(self):
+    # def test_run_kitfzkhaus_spaces_full_layers_full(self):
     #     """Run project with AC20-FZK-Haus.ifc"""
     #     ifc = 'AC20-FZK-Haus.ifc'
-    #     # test -> spaces-full, layer-full
     #     answers = (True, True, 'Kitchen - preparations, storage', True, 'Concrete_DK', True, 'hardwood', True,
     #                'Light_Concrete_DK', True, 'solid_brick_a', 'heavy', 1, 'Brick', 'brick_H', 'EnEv', 1, 'Glas',
     #                'glas_generic')
@@ -84,10 +77,9 @@ class TestIntegrationTEASER(IntegrationBase, unittest.TestCase):
     #         return_code = self.run_project(ifc, 'TEASER')
     #     self.assertEqual(0, return_code, "Project did not finish successfully.")
 
-    # def test_run_kitoffice_test4(self):
+    # def test_run_kitoffice_spaces_full_layers_full(self):
     #     """Run project with AC20-Institute-Var-2.ifc"""
     #     ifc = 'AC20-Institute-Var-2.ifc'
-    #     # test -> spaces-full, layer-full
     #     answers = (True, True, True, 'belgian_brick', 'Glas', True, 'glas_generic', 500, 1.5, 0.2, True, 'air_layer',
     #                0.1, True, 'Concrete_DK', 2015, 'heavy', 1, 'Beton', 'Light_Concrete_DK', 1, 'Beton', 1, 'Door', 1,
     #                'Beton')
