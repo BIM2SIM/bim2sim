a1bab4071b9234cbb08b54ba4adbfa02bbfd46f9
Veronika Richter <veronika.richter@rwth-aachen.de>
2021-04-23 16:53:57 +0200

    Update DigitalHub Model to latest (almost final, but working) version

--- MainLib/bim2sim/__init__.py
+++ MainLib/bim2sim/__init__.py
@@ -170,7 +170,6 @@ def _debug_run_bps_ep():
     # rel_example = 'ExampleFiles/AC20-FZK-Haus.ifc'
     # rel_example = 'ResultFiles/AC20-Institute-Var-2_with_SB11.ifc' # aktuell
     # rel_example = 'ResultFiles/AC20-FZK-Haus_with_SB44.ifc' # aktuell
-    # rel_example = 'ResultFiles/FM_ARC_DigitalHub_with_SB11.ifc'
     # rel_example = 'ResultFiles/Proposal_1_Storey_SpaceBoundaries_with_SB.ifc'
     # rel_example = 'ResultFiles/2020-10-15-KHH-Test_with_SB.ifc'
     rel_example = 'ExampleFiles/AC20-Institute-Var-2.ifc'
