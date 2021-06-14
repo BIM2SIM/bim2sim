ccb8f909ec5431b98e1ae8f2b1d9def2a2c75cf3
Christian <cwarnecke@rom-technik.de>
2021-06-14 19:24:18 +0200

    remove task FixPorts

--- PluginHKESim/bim2sim_hkesim/hkesim.py
+++ PluginHKESim/bim2sim_hkesim/hkesim.py
@@ -24,7 +24,6 @@ class PluginHKESim(Plugin):
     default_tasks = [
         hvac.SetIFCTypesHVAC,
         common.LoadIFC,
-        # hvac.FixPorts,
         common.CreateElements,
         hvac.ConnectElements,
         hvac.MakeGraph,
