ec735be472e4c562e72da35fd7ae7ba64e497057
Diego Cordoba <diego.cordoba@eonerc.rwth-aachen.de>
2021-07-16 11:10:59 +0200

    enrich building by template for thread save corrected

--- MainLib/bim2sim/__init__.py
+++ MainLib/bim2sim/__init__.py
@@ -240,7 +240,7 @@ setup_default()
 
 if __name__ == '__main__':
     # _debug_run_cfd()
-    # _debug_run_bps()
+    _debug_run_bps()
     # _debug_run_bps_ep()
-    _debug_run_hvac()
+    # _debug_run_hvac()
 
--- MainLib/bim2sim/task/bps/enrich_non_valid.py
+++ MainLib/bim2sim/task/bps/enrich_non_valid.py
@@ -22,6 +22,7 @@ class EnrichNonValid(ITask):
         self.enriched_layers = []
         self.enriched_class = {}
         self.window_enrichment = {}
+        self.instance_template = {}
         pass
 
     def run(self, workflow, invalid_layers, instances):
@@ -45,7 +46,7 @@ class EnrichNonValid(ITask):
     def layers_creation(self, instance, construction_type, instances):
         if len(instance.layers) == 0:
             yield from EnrichBuildingByTemplates.template_layers_creation(
-                instance, construction_type, instances)
+                instance, construction_type, instances, self.instance_template)
         else:
             yield from self.manual_layers_creation(instance)
 
