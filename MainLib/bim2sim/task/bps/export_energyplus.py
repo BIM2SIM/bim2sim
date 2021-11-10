432849478994c3379e0ec8f47849d03c4e975295
Veronika Richter <veronika.richter@rwth-aachen.de>
2021-11-10 14:59:34 +0100

    add visualization tools for energyplus

--- MainLib/bim2sim/task/bps/export_energyplus.py
+++ MainLib/bim2sim/task/bps/export_energyplus.py
@@ -1193,46 +1193,6 @@ class ExportEP(ITask):
                 continue
             neighbors = this_obj.bound_neighbors
 
-    @staticmethod
-    def _display_shape_of_space_boundaries(instances):
-        """Display topoDS_shapes of space boundaries"""
-        display, start_display, add_menu, add_function_to_menu = init_display()
-        colors = ['blue', 'red', 'magenta', 'yellow', 'green', 'white', 'cyan']
-        col = 0
-        for inst in instances:
-            if instances[inst].ifc.is_a('IfcRelSpaceBoundary'):
-                col += 1
-                bound = instances[inst]
-                if bound.bound_instance is None:
-                    continue
-                if not bound.bound_instance.ifc.is_a("IfcWall"):
-                    pass
-                try:
-                    display.DisplayShape(bound.bound_shape, color=colors[(col - 1) % len(colors)])
-                except:
-                    continue
-        display.FitAll()
-        start_display()
-
-    @staticmethod
-    def _display_bound_normal_orientation(instances):
-        display, start_display, add_menu, add_function_to_menu = init_display()
-        col = 0
-        for inst in instances:
-            if not instances[inst].ifc.is_a('IfcSpace'):
-                continue
-            space = instances[inst]
-            for bound in space.space_boundaries:
-                face_towards_center = space.space_center.XYZ() - bound.bound_center
-                face_towards_center.Normalize()
-                dot = face_towards_center.Dot(bound.bound_normal)
-                if dot > 0:
-                    display.DisplayShape(bound.bound_shape, color="red")
-                else:
-                    display.DisplayShape(bound.bound_shape, color="green")
-        display.FitAll()
-        start_display()
-
     def export_bounds_to_stl(self, instances, stl_name):
         """
         This function exports a space to an idf file.
