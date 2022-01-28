4f9ac2b41478e045d01b6b4fe8b5a1339b696141
David <david.jansen1@rwth-aachen.de>
2022-01-28 13:25:02 +0100

    fix: check for correct os name

--- PluginCFD/test_cfd/test_cfd_integration.py
+++ PluginCFD/test_cfd/test_cfd_integration.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import warnings
 
 from bim2sim import workflow
 from bim2sim.decision.decisionhandler import DebugDecisionHandler
@@ -15,7 +16,7 @@ class TestIntegrationCFD(IntegrationBaseCFD, unittest.TestCase):
 
     # @unittest.skip("")
     def test_run_kitfzkhaus_spaces_low_layers_low(self):
-        if os.name == 'Linux':
+        if os.name == 'posix':  # only linux
             """Run project with AC20-FZK-Haus.ifc"""
             ifc = 'AC20-FZK-Haus.ifc'
             used_workflow = workflow.CFDWorkflowDummy()
@@ -27,3 +28,5 @@ class TestIntegrationCFD(IntegrationBaseCFD, unittest.TestCase):
                 decision.value = answer
             self.assertEqual(0, handler.return_value,
                              "Project did not finish successfully.")
+        else:
+            warnings.warn("Current OS not linux. This test will be skipped.")
