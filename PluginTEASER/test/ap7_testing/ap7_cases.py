6593c84b287a82892a7786e52de5b50cf7f39d1b
David <david.jansen1@rwth-aachen.de>
2022-01-28 10:58:23 +0100

    rename tests

--- PluginTEASER/test/ap7_testing/ap7_cases.py
+++ PluginTEASER/test/ap7_testing/ap7_cases.py
@@ -12,7 +12,7 @@ RESULT_PATH = Path(__file__).parent.parent.parent.parent / 'ResultFiles'
 
 class IntegrationBaseTEASER(IntegrationBaseTeaserInteractive, unittest.TestCase):
 
-    def ap7_run_kitfzkhaus_spaces_low_layers_low(self):
+    def test_ap7_run_kitfzkhaus_spaces_low_layers_low(self):
         """Run project with AC20-FZK-Haus.ifc"""
         ifc = EXAMPLE_PATH / 'AC20-FZK-Haus.ifc'
         used_workflow = workflow.BPSMultiZoneAggregatedLayersLowSimulation()
@@ -26,7 +26,7 @@ class IntegrationBaseTEASER(IntegrationBaseTeaserInteractive, unittest.TestCase)
         self.assertEqual(0, handler.return_value,
                          "Project did not finish successfully.")
 
-    def ap7_run_DigitalHub_spaces_low_layers_low(self):
+    def test_ap7_run_DigitalHub_spaces_low_layers_low(self):
         """Run project with AC20-Institute-Var-2.ifc"""
         ifc = RESULT_PATH / 'FM_ARC_DigitalHub_fixed002.ifc'
         used_workflow = workflow.BPSMultiZoneAggregatedLayersLowSimulation()
@@ -39,7 +39,7 @@ class IntegrationBaseTEASER(IntegrationBaseTeaserInteractive, unittest.TestCase)
         self.assertEqual(0, handler.return_value,
                          "Project did not finish successfully.")
 
-    def ap7_run_DigitalHub_spaces_medium_layers_low(self):
+    def test_ap7_run_DigitalHub_spaces_medium_layers_low(self):
         """Run project with AC20-Institute-Var-2.ifc"""
         ifc = RESULT_PATH / 'FM_ARC_DigitalHub_fixed002.ifc'
         used_workflow = workflow.BPSMultiZoneAggregatedLayersLowSimulation()
@@ -52,7 +52,7 @@ class IntegrationBaseTEASER(IntegrationBaseTeaserInteractive, unittest.TestCase)
         self.assertEqual(0, handler.return_value,
                          "Project did not finish successfully.")
 
-    def ap7_run_DigitalHub_spaces_full_layers_low(self):
+    def test_ap7_run_DigitalHub_spaces_full_layers_low(self):
         """Run project with AC20-Institute-Var-2.ifc"""
         ifc = RESULT_PATH / 'FM_ARC_DigitalHub_fixed002.ifc'
         used_workflow = workflow.BPSMultiZoneAggregatedLayersLowSimulation()
