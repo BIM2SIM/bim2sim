d1854c8e43d2a1efde121cc4ce6166d5d45f5cf7
David <david.jansen1@rwth-aachen.de>
2022-03-10 11:43:29 +0100

    add pillow to requirements

--- PluginTEASER/bim2sim_teaser/test/ap7_testing/ap7_cases.py
+++ PluginTEASER/bim2sim_teaser/test/ap7_testing/ap7_cases.py
@@ -13,7 +13,8 @@ RESULT_PATH = Path(__file__).parent.parent.parent.parent.parent / 'ResultFiles'
 class IntegrationBaseTEASER(IntegrationBaseTeaserInteractive, unittest.TestCase):
 
     def test_ap7_run_kitfzkhaus_spaces_medium_layers_low(self):
-        """Run project with AC20-FZK-Haus.ifc"""
+        """Run project with AC20-FZK-Haus.ifc with with modified
+        materials and one zone aggregation."""
         ifc = EXAMPLE_PATH / 'AC20-FZK-Haus.ifc'
         used_workflow = workflow.BPSMultiZoneAggregatedLayersLowSimulation()
         project = self.create_project(ifc, 'TEASER', used_workflow)
@@ -27,7 +28,8 @@ class IntegrationBaseTEASER(IntegrationBaseTeaserInteractive, unittest.TestCase)
                          "Project did not finish successfully.")
 
     def test_ap7_run_DigitalHub_spaces_low_layers_low(self):
-        """Run project with AC20-Institute-Var-2.ifc"""
+        """Run project with FM_ARC_DigitalHub_fixed002.ifc with modified
+        materials and one zone aggregation."""
         ifc = RESULT_PATH / 'FM_ARC_DigitalHub_fixed002.ifc'
         used_workflow = workflow.BPSMultiZoneAggregatedLayersLowSimulation()
         project = self.create_project(ifc, 'TEASER', used_workflow)
@@ -40,12 +42,14 @@ class IntegrationBaseTEASER(IntegrationBaseTeaserInteractive, unittest.TestCase)
                          "Project did not finish successfully.")
 
     def test_ap7_run_DigitalHub_spaces_medium_layers_low(self):
-        """Run project with AC20-Institute-Var-2.ifc"""
+        """Run project with FM_ARC_DigitalHub_fixed002.ifc with modified
+        materials and one multizone aggregation."""
         ifc = RESULT_PATH / 'FM_ARC_DigitalHub_fixed002.ifc'
         used_workflow = workflow.BPSMultiZoneAggregatedLayersLowSimulation()
         project = self.create_project(ifc, 'TEASER', used_workflow)
         answers = ('ARCHICAD-64', *(None,) * 150, True, True,
-                   2015, 'heavy', 'Waermeschutzverglasung, dreifach', 'by_all_criteria')
+                   2015, 'heavy', 'Waermeschutzverglasung, dreifach',
+                   'by_all_criteria')
         handler = DebugDecisionHandler(answers)
         for decision, answer in handler.decision_answer_mapping(project.run()):
             decision.value = answer
@@ -53,7 +57,8 @@ class IntegrationBaseTEASER(IntegrationBaseTeaserInteractive, unittest.TestCase)
                          "Project did not finish successfully.")
 
     def test_ap7_run_DigitalHub_spaces_full_layers_low(self):
-        """Run project with AC20-Institute-Var-2.ifc"""
+        """Run project with FM_ARC_DigitalHub_fixed002.ifc with adding
+        material enrichment and without zone aggregation."""
         ifc = RESULT_PATH / 'FM_ARC_DigitalHub_fixed002.ifc'
         used_workflow = workflow.BPSMultiZoneAggregatedLayersLowSimulation()
         project = self.create_project(ifc, 'TEASER', used_workflow)
