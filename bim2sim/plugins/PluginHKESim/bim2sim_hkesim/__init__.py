87ba065c9bc36ee855028db1eb8c007dd410f0b7
Christian <cwarnecke@rom-technik.de>
2022-05-11 15:15:15 +0200

    update hkesim plugin

--- bim2sim/plugins/PluginHKESim/bim2sim_hkesim/__init__.py
+++ bim2sim/plugins/PluginHKESim/bim2sim_hkesim/__init__.py
@@ -1,10 +1,37 @@
-﻿'''HKESim plugin for bim2sim
+﻿"""HKESim plugin for bim2sim
 
 Holds logic to run a simulation based on prepared ifc data
-'''
- 
-def get_hkesim():
-    from .hkesim import PluginHKESim
-    return PluginHKESim
+"""
+from bim2sim.export.modelica import standardlibrary
+from bim2sim.kernel.elements import hvac as hvac_elements
+from bim2sim.workflow import PlantSimulation
+from bim2sim.plugins import Plugin
 
-CONTEND = {'hkesim':get_hkesim}
+from models import HKESim
+from task import base, common, hvac
+
+
+class LoadLibrariesHKESim(base.ITask):
+    """Load HKESim library for export"""
+    touches = ('libraries', )
+
+    def run(self, workflow, **kwargs):
+        return (standardlibrary.StandardLibrary, HKESim),
+
+
+class PluginHKESim(Plugin):
+    name = 'HKESim'
+    default_workflow = PlantSimulation
+    tasks = {LoadLibrariesHKESim}
+    elements = {*hvac_elements.items}
+    default_tasks = [
+        common.LoadIFC,
+        hvac.CheckIfcHVAC,
+        common.CreateElements,
+        hvac.ConnectElements,
+        hvac.MakeGraph,
+        hvac.Reduce,
+        hvac.DeadEnds,
+        LoadLibrariesHKESim,
+        hvac.Export,
+    ]
