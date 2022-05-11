c20502c9f68a8f10ad925fa14b34fddf18a87d6c
Christian <cwarnecke@rom-technik.de>
2022-05-11 15:41:50 +0200

    update plugin ep

--- bim2sim/plugins/PluginEnergyPlus/bim2sim_energyplus/__init__.py
+++ bim2sim/plugins/PluginEnergyPlus/bim2sim_energyplus/__init__.py
@@ -1,10 +1,39 @@
-﻿'''EnergyPlus plugin for bim2sim
+﻿"""EnergyPlus plugin for bim2sim
 
 Holds logic to run a simulation based on prepared ifc data
-'''
+"""
+from bim2sim.kernel.elements import bps as bps_elements
+from bim2sim.plugins import Plugin
+from bim2sim.task import common, bps
+from bim2sim.workflow import BPSMultiZoneSeparatedEP
 
-def get_energyplus():
-    from .energyplus import EnergyPlus
-    return EnergyPlus
+import task as ep_tasks
 
-CONTEND = {'energyplus':get_energyplus}
+
+class EnergyPlus(Plugin):
+    name = 'EnergyPlus'
+    default_workflow = BPSMultiZoneSeparatedEP
+    elements = {*bps_elements.items}
+    default_tasks = [
+        common.LoadIFC,
+        common.CreateElements,
+        bps.CreateSpaceBoundaries,
+        bps.Prepare,
+        bps.EnrichUseConditions,
+        bps.OrientationGetter,
+        bps.MaterialVerification,  # LOD.full
+        bps.EnrichMaterial,  # LOD.full
+        bps.BuildingVerification,  # all LODs
+        bps.EnrichNonValid,  # LOD.full
+        bps.EnrichBuildingByTemplates,  # LOD.low
+        bps.DisaggregationCreation,
+        bps.BindThermalZones,
+        ep_tasks.IfcValidation,
+        ep_tasks.EPGeomPreprocessing,
+        ep_tasks.AddSpaceBoundaries2B,
+        ep_tasks.WeatherEnergyPlus,
+        ep_tasks.CreateIdf,
+        ep_tasks.IdfPostprocessing,
+        ep_tasks.ExportIdfForCfd,
+        ep_tasks.RunEnergyPlusSimulation,
+    ]
