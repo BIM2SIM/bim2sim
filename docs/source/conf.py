285625eabeb9642b81f97377f99984303f5dd3aa
David <david.jansen1@rwth-aachen.de>
2022-10-26 11:52:49 +0200

    change project name to lowercase

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'BIM2SIM'
+project = 'bim2sim '
 copyright = '2022, RWTH Aachen University, EBC & E3D; ROM Technik GmbH'
 # author = 'David Jansen (todo)'
 
