4aa9397ae96e8ee311d55eabeae49741b5bf72a9
David <david.jansen1@rwth-aachen.de>
2022-11-28 17:13:29 +0100

    fix test decisions answers

--- bim2sim/plugins/PluginAixLib/test/integration_test/test_hvac.py
+++ bim2sim/plugins/PluginAixLib/test/integration_test/test_hvac.py
@@ -17,55 +17,30 @@ class IntegrationBaseAixLib(IntegrationBase):
 class TestIntegrationAixLib(IntegrationBaseAixLib, unittest.TestCase):
 
     def test_vereinshaus1_aixlib(self):
-        """Run project with KM_DPM_Vereinshaus_Gruppe62_Heizung_with_pumps.ifc"""
+        """Run project with
+        KM_DPM_Vereinshaus_Gruppe62_Heizung_with_pumps.ifc"""
         ifc = 'KM_DPM_Vereinshaus_Gruppe62_Heizung_with_pumps.ifc'
         project = self.create_project(ifc, 'aixlib')
         answers = ('HVAC-HeatPump', 'HVAC-Storage', 'HVAC-Storage',
                    '2lU4kSSzH16v7KPrwcL7KZ', '0t2j$jKmf74PQpOI0ZmPCc',
-                   *(True,)*13, 0.75, 150, *(5,)*11)
-
+                   # 1x expansion tank and 16x dead end
+                   *(True,)*17,
+                   # boiler efficiency
+                   0.9,
+                   # boiler power
+                   150,
+                   # current, height, voltage, vol_flow of pump
+                   *(2, 5, 230, 1) * 2,
+                   # power of space heaters
+                   *(1,) * 11)
         handler = DebugDecisionHandler(answers)
         for decision, answer in handler.decision_answer_mapping(project.run()):
             decision.value = answer
         self.assertEqual(0, handler.return_value,
                          "Project did not finish successfully.")
 
-    def test_vereinshaus2_aixlib(self):
-        """Run project with KM_DPM_Vereinshaus_Gruppe62_Heizung_DTV_all_elements.ifc"""
-        ifc = 'KM_DPM_Vereinshaus_Gruppe62_Heizung_DTV_all_elements.ifc'
-        project = self.create_project(ifc, 'aixlib')
-        answers = ('HVAC-HeatPump', 'HVAC-Storage', 'HVAC-Storage',
-                   '0k0IjzL0z6aOYAX23H_dA5', '1U379nXO902R21a41MGQRw',
-                   *(True,)*15, 0.75, 150, *(5,)*11)
-        handler = DebugDecisionHandler(answers)
-        for decision, answer in handler.decision_answer_mapping(project.run()):
-            decision.value = answer
-        self.assertEqual(0, handler.return_value,
-                         "Project did not finish successfully.")
-
-    def test_b03_heating_aixlib(self):
-        """Run project with B03_Heating.ifc"""
-        ifc = 'B03_Heating.ifc'
-        project = self.create_project(ifc, 'aixlib')
-
-        answers = (*('HVAC-Valve',)*2, 'HVAC-Distributor',
-                   'HVAC-Boiler', 'HVAC-Storage', *('HVAC-Valve',)*14,
-                   '2PFOreSeyfWqxUJNMz5nFO', '2YKblmYbhnh4RrfqKcCxPJ',
-                   *(True,) * 13, 0.75, 50, 150, 70, *(1, 500,) * 7)
-                   # *(True,) * 21, 0.75, 45, 150, 65, 7, *(1,) * 7)
-
-        handler = DebugDecisionHandler(answers)
-        for decision, answer in handler.decision_answer_mapping(project.run()):
-            decision.value = answer
-
-        graph = project.playground.state['graph']
-        aggregated = Counter((type(item) for item in graph.element_graph.nodes))
-        self.assertIn(ConsumerHeatingDistributorModule, aggregated)
-        self.assertEqual(0, handler.return_value,
-                         "Project did not finish successfully.")
-
-    def test_run_b03_heating_new(self):
-        """Run project with B03_Heating.ifc"""
+    def test_run_b03_heating(self):
+        """Run project with 2022_11_21_B03_Heating_ownCells"""
         ifc = '2022_11_21_B03_Heating_ownCells.ifc'
         project = self.create_project(ifc, 'aixlib')
         project.workflow.aggregations = [
@@ -77,16 +52,11 @@ class TestIntegrationAixLib(IntegrationBaseAixLib, unittest.TestCase):
             'ConsumerHeatingDistributorModule',
             # 'GeneratorOneFluid'
         ]
-        # handler = ConsoleDecisionHandler()
-        # handler.handle(project.run())
-        # answers = (*('HVAC-Valve',) * 2, 'HVAC-Distributor',
-        #            'HVAC-Boiler', 'HVAC-Storage', *('HVAC-Valve',) * 14,
-        #            '2PFOreSeyfWqxUJNMz5nFO', '2YKblmYbhnh4RrfqKcCxPJ',
-        #            *(True,) * 13, 0.75, 50, 150, 70, *(1, 500,) * 7)
-        answers = ('HVAC-Distributor', *('HVAC-ThreeWayValve',) * 2,  *('HVAC-Valve',) * 14, *(None,) * 2,
-                   '1gCa_YEgd8WK0YER$738Ii', '184XXDHbkqkqSUK7orFEGw',
+        answers = ('HVAC-Distributor', *('HVAC-ThreeWayValve',) * 2,
+                   *('HVAC-Valve',) * 14, *(None,) * 2,
                    *(True,) * 5, 0.75, 50,
-                   # rated current, rated height, rated_voltage, rated_volume_floow
+                   # rated current, rated height, rated_voltage,
+                   # rated_volume_flow for pumps
                     150, 70, 50, 50,
                    # body mass and heat capacity for all space heaters
                    *(1, 500,) * 7)
--- bim2sim/plugins/PluginHKESim/test/test_hvac.py
+++ bim2sim/plugins/PluginHKESim/test/test_hvac.py
@@ -11,32 +11,40 @@ from bim2sim.utilities.test import IntegrationBase
 class TestIntegrationHKESIM(IntegrationBase, unittest.TestCase):
 
     def test_run_vereinshaus1(self):
-        """Run project with KM_DPM_Vereinshaus_Gruppe62_Heizung_with_pumps.ifc"""
+        """Run project with
+        KM_DPM_Vereinshaus_Gruppe62_Heizung_with_pumps.ifc"""
         ifc = 'KM_DPM_Vereinshaus_Gruppe62_Heizung_with_pumps.ifc'
         project = self.create_project(ifc, 'hkesim')
         answers = ('HVAC-HeatPump', 'HVAC-Storage', 'HVAC-Storage',
                    '2lU4kSSzH16v7KPrwcL7KZ', '0t2j$jKmf74PQpOI0ZmPCc',
-                   *(True,)*15,
-                   200, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5)
+                   *(True,)*17,
+                   # boiler efficiency
+                   0.9,
+                   # boiler power
+                   150,
+                   # current, height, voltage, vol_flow of pump
+                   *(2, 5, 230, 1) * 2,
+                   # power of space heaters
+                   *(1,) * 11)
         handler = DebugDecisionHandler(answers)
         for decision, answer in handler.decision_answer_mapping(project.run()):
             decision.value = answer
         self.assertEqual(0, handler.return_value,
                          "Project did not finish successfully.")
 
-    def test_run_vereinshaus2(self):
-        """Run project with KM_DPM_Vereinshaus_Gruppe62_Heizung_DTV_all_elements.ifc"""
-        ifc = 'KM_DPM_Vereinshaus_Gruppe62_Heizung_DTV_all_elements.ifc'
-        project = self.create_project(ifc, 'hkesim')
-        answers = ('HVAC-HeatPump', 'HVAC-Storage', 'HVAC-Storage',
-                   '0k0IjzL0z6aOYAX23H_dA5', '1U379nXO902R21a41MGQRw',
-                   *(True,)*13,
-                   200, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5)
-        handler = DebugDecisionHandler(answers)
-        for decision, answer in handler.decision_answer_mapping(project.run()):
-            decision.value = answer
-        self.assertEqual(0, handler.return_value,
-                         "Project did not finish successfully.")
+    # def test_run_vereinshaus2(self):
+    #     """Run project with KM_DPM_Vereinshaus_Gruppe62_Heizung_DTV_all_elements.ifc"""
+    #     ifc = 'KM_DPM_Vereinshaus_Gruppe62_Heizung_DTV_all_elements.ifc'
+    #     project = self.create_project(ifc, 'hkesim')
+    #     answers = ('HVAC-HeatPump', 'HVAC-Storage', 'HVAC-Storage',
+    #                '0k0IjzL0z6aOYAX23H_dA5', '1U379nXO902R21a41MGQRw',
+    #                *(True,)*13,
+    #                200, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5)
+    #     handler = DebugDecisionHandler(answers)
+    #     for decision, answer in handler.decision_answer_mapping(project.run()):
+    #         decision.value = answer
+    #     self.assertEqual(0, handler.return_value,
+    #                      "Project did not finish successfully.")
 
     def test_run_b03_heating(self):
         """Run project with B03_Heating.ifc"""
@@ -49,13 +57,8 @@ class TestIntegrationHKESIM(IntegrationBase, unittest.TestCase):
             'ParallelPump',
             'ConsumerHeatingDistributorModule',
         ]
-        # handler = ConsoleDecisionHandler()
-        # handler.handle(project.run())
-        # answers = (*('HVAC-Valve',) * 2, 'HVAC-Distributor',
-        #            'HVAC-Boiler', 'HVAC-Storage', *('HVAC-Valve',) * 14,
-        #            '2PFOreSeyfWqxUJNMz5nFO', '2YKblmYbhnh4RrfqKcCxPJ',
-        #            *(True,) * 13, 0.75, 50, 150, 70, *(1, 500,) * 7)
-        answers = ('HVAC-Distributor', *('HVAC-ThreeWayValve',) * 2,  *('HVAC-Valve',) * 14, *(None,) * 2,
+        answers = ('HVAC-Distributor', *('HVAC-ThreeWayValve',) * 2,
+                   *('HVAC-Valve',) * 14, *(None,) * 2,
                    *(True,) * 5, 0.75, 50, 150, 70, *(1, 500,) * 7)
         handler = DebugDecisionHandler(answers)
         for decision, answer in handler.decision_answer_mapping(project.run()):
@@ -66,7 +69,3 @@ class TestIntegrationHKESIM(IntegrationBase, unittest.TestCase):
         self.assertIn(ConsumerHeatingDistributorModule, aggregated)
         self.assertEqual(0, handler.return_value,
                          "Project did not finish successfully.")
-
-
-class TestIntegrationAixLib(unittest.TestCase):
-    pass
