bf7a0c9e7908d563c502d60f3beb8967434233de
David <david.jansen1@rwth-aachen.de>
2022-11-29 16:25:31 +0100

    add changes to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
-import glob
 
 
-data_files = []
-directories = glob.glob('bim2sim\\assets\\')
-for directory in directories:
-    files = glob.glob(directory + '*')
-    data_files.append((directory, files))
+
+# data_files = []
+# directories = glob.glob('bim2sim\\assets\\')
+# for directory in directories:
+#     files = glob.glob(directory + '*')
+#     data_files.append((directory, files))
 
 with open("README.md", 'r') as f:
     long_description = f.read()
@@ -20,21 +20,20 @@ setup(
     long_description_content_type='text/markdown',
     author='BIM2SIM',
     author_email='???',
-    url="???",
+    url="https://github.com/BIM2SIM/bim2sim",
     packages=find_packages() + ['bim2sim.assets'],
     include_package_data=True,
-    data_files=data_files,
-    # package_data={'': ['assets/*.*']},
-    python_requires='>=3.9.0',
+    # data_files=data_files,
+    package_data={'': ['bim2sim/assets/*.*']},
+    python_requires='>=3.8.*,<3.10.*',
     install_requires=[
         'docopt', 'numpy', 'python-dateutil',
         'mako', 'networkx>=2.2', 'pint', 'pandas',
-        'deep_translator', 'matplotlib'
+        'deep_translator', 'matplotlib' # TODO 'occ-utils'
     ],  # external packages as dependencies
     extras_require={
-        'manual_install': ['ifcopenshell>=0.6'],
-        'plotting': ['matplotlib'],
-        'communication': ['rpyc'],
+        'manual_install': ['ifcopenshell>=0.6', '', ],  # TODO occ-core
+        # 'plotting': ['matplotlib'],
     },
     entry_points={
         'console_scripts': [
