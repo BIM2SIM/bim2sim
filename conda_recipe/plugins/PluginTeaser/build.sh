493cd4d26509e5bc5c66e61f0ea298e6555c3825
Sven-Heiko Hinrichs <sven.hinrichs@rwth-aachen.de>
2023-02-05 11:55:14 +0100

    ci_build_dev_plugin

--- conda_recipe/plugins/PluginTeaser/build.sh
+++ conda_recipe/plugins/PluginTeaser/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pip install pyqt5==5.12.2
+pip install pyqt5==5.15.2
 pip install git+https://github.com/RWTH-EBC/TEASER@development
 pip install git+https://github.com/DaJansenGit/BuildingsPy.git
