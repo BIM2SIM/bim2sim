3ef2615249cbb23828d8dbbf9c2d5f572b307275
Sven-Heiko Hinrichs <sven.hinrichs@rwth-aachen.de>
2023-03-11 00:26:14 +0100

    ci_build_dev

--- conda_recipe/basic_build/base_run_test.sh
+++ conda_recipe/basic_build/base_run_test.sh
@@ -5,7 +5,7 @@ pip install coverage
 pip install coverage-badge
 wget --quiet  -O test/TestModels/download.zip https://rwth-aachen.sciebo.de/s/R6K1H5Z9fiB3EoB/download
 unzip test/TestModels/download.zip -d test/TestModels/
-coverage run -m unittest discover /test
+coverage run -m unittest discover test
 coverage report -i
 mkdir -p $CI_COMMIT_REF_NAME/coverage
 coverage html -i
