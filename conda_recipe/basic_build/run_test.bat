b2fbded4949b9a4a78f65bae7ec9011a2c188fb8
Sven-Heiko Hinrichs <sven.hinrichs@rwth-aachen.de>
2023-03-10 21:18:57 +0100

    ci_build_dev

---
+++ conda_recipe/basic_build/run_test.bat
@@ -0,0 +1,5 @@
+pip install coverage
+pip install coverage-badge
+wget  --quiet -O test/TestModels/download.zip https://rwth-aachen.sciebo.de/s/R6K1H5Z9fiB3EoB/download
+coverage run -m unittest discover test
+coverage report -i
--- conda_recipe/plugins/PluginAixLib/plugin_aixlib_test.bat
+++ conda_recipe/plugins/PluginAixLib/plugin_aixlib_test.bat
@@ -1,12 +1,8 @@
 pip install coverage
 pip install coverage-badge
 
-mv ./* /bim2sim-coding/
-cd /bim2sim-coding
 wget --quiet -O /bim2sim-coding/test/TestModels/download.zip https://rwth-aachen.sciebo.de/s/R6K1H5Z9fiB3EoB/download
 unzip /bim2sim-coding/test/TestModels/download.zip -d /bim2sim-coding/test/TestModels/
-coverage run -m unittest discover /bim2sim-coding/test
+coverage run -m unittest discover /bim2sim-coding/bim2sim/plugins/PluginAixLib/test/integration_test
 coverage report -i
-mkdir -p $CI_COMMIT_REF_NAME/coverage
 coverage html -i
-cp htmlcov/* $CI_COMMIT_REF_NAME/coverage/
