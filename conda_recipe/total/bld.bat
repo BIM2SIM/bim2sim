e333ec8b50eca53f0d7272617f8a1f3c08564691
Sven-Heiko Hinrichs <sven.hinrichs@rwth-aachen.de>
2023-03-10 11:52:43 +0100

    ci_build_dev

--- conda_recipe/total/bld.bat
+++
@@ -1,8 +0,0 @@
-@echo on
-conda install conda-bld/bim2sim/linux-64/bim2sim-*
-conda install conda-bld/bim2sim_cfd/linux-64/bim2sim_cfd-*
-conda install conda-bld/bim2sim_aixlib/linux-64/bim2sim_aixlib-*
-conda install conda-bld/bim2sim_energyplus/linux-64/bim2sim_energyplus-*
-conda install conda-bld/bim2sim_lca/linux-64/bim2sim_lca-*
-conda install conda-bld/bim2sim_teaser/linux-64/bim2sim_teaser-*
-IF %ERRORLEVEL% NEQ 0 exit 1
