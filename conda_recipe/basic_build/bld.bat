ffff3c446c3b4695d9be2fc478c71d4f11893765
Sven-Heiko Hinrichs <sven.hinrichs@rwth-aachen.de>
2023-03-11 21:22:31 +0100

    ci_build_dev

--- conda_recipe/basic_build/bld.bat
+++ conda_recipe/basic_build/bld.bat
@@ -1,5 +1,5 @@
 @echo on
 python -m pip install --no-deps --ignore-installed .
-pip install deep_translator
+pip install deep-translator
 pip install decision
 IF %ERRORLEVEL% NEQ 0 exit 1
--- conda_recipe/basic_build/build.sh
+++ conda_recipe/basic_build/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python -m pip install --no-deps --ignore-installed .
-pip install deep_translator
+pip install deep-translator
 pip install decision
 
