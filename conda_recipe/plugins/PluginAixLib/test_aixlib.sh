f29571f05b94513ffeac2930db18333d800ea5da
Sven-Heiko Hinrichs <sven.hinrichs@rwth-aachen.de>
2023-03-12 20:25:53 +0100

    ci_build_dev

--- conda_recipe/plugins/PluginAixLib/test_aixlib.sh
+++ conda_recipe/plugins/PluginAixLib/test_aixlib.sh
@@ -9,5 +9,3 @@ pip install decision
 wget --quiet -O test/TestModels/download.zip https://rwth-aachen.sciebo.de/s/R6K1H5Z9fiB3EoB/download
 unzip test/TestModels/download.zip -d test/TestModels/
 coverage run -m unittest discover bim2sim/plugins/PluginAixLib/test/integration_test
-coverage report -i
-coverage html -i
--- conda_recipe/plugins/PluginCFD/test_cfd.sh
+++ conda_recipe/plugins/PluginCFD/test_cfd.sh
@@ -9,5 +9,3 @@ pip install decision
 wget --quiet -O test/TestModels/download.zip https://rwth-aachen.sciebo.de/s/SAUQQgvwqeS96ix/download
 unzip test/TestModels/download.zip -d test/TestModels/
 coverage run -m unittest discover bim2sim/plugins/PluginCFD/test/integration_test
-coverage report -i
-coverage html -i
