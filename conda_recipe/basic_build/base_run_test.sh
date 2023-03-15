74cf5d3124d03cedc3048ad3d20fd03effcedd7b
Sven-Heiko Hinrichs <sven.hinrichs@rwth-aachen.de>
2023-03-15 11:22:29 +0100

    ci_build_dev

--- conda_recipe/basic_build/base_run_test.sh
+++ conda_recipe/basic_build/base_run_test.sh
@@ -1,9 +1,9 @@
 set -x
-apt-get update -y && \
-apt-get upgrade -y && \
-apt-get install unzip wget -y && \
-pip install coverage && \
-pip install coverage-badge && \
-wget --quiet  -O test/TestModels/download.zip https://rwth-aachen.sciebo.de/s/R6K1H5Z9fiB3EoB/download && \
-unzip test/TestModels/download.zip -d test/TestModels/ && \
+apt-get update -y
+apt-get upgrade -y
+apt-get install unzip wget -y
+pip install coverage
+pip install coverage-badge
+wget --quiet  -O test/TestModels/download.zip https://rwth-aachen.sciebo.de/s/R6K1H5Z9fiB3EoB/download
+unzip test/TestModels/download.zip -d test/TestModels/
 coverage run -m unittest discover test
