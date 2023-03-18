fc6fcfcc1f65b48bfce91ea97b21618696bd6b90
Sven-Heiko Hinrichs <sven.hinrichs@rwth-aachen.de>
2023-03-18 20:20:29 +0100

    ci_build_dev

--- conda_recipe/plugins/PluginLCA/test_lca.sh
+++ conda_recipe/plugins/PluginLCA/test_lca.sh
@@ -7,6 +7,6 @@ pip install coverage &&
 pip install coverage-badge &&
 wget --quiet -O test/TestModels/download.zip https://rwth-aachen.sciebo.de/s/SAUQQgvwqeS96ix/download &&
 unzip test/TestModels/download.zip -d test/TestModels/ &&
-coverage run -m unittest discover bim2sim/plugins/PluginLCA/test/integration_test &&
-coverage report -i &&
+coverage run -m unittest discover bim2sim/plugins/PluginLCA/test/integration_test ||
+coverage report -i ||
 coverage html -i
