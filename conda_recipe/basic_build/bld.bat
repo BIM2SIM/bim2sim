72a5b9d8c55c64998036f02ec96a2606d13d416f
Sven-Heiko Hinrichs <sven.hinrichs@rwth-aachen.de>
2023-03-24 20:13:51 +0100

    ci_build_dev

---
+++ conda_recipe/basic_build/bld.bat
@@ -0,0 +1,7 @@
+@echo on
+python -m pip install --no-deps --ignore-installed .
+pip install https://files.pythonhosted.org/packages/ea/4e/575bbb915054e2875acba262568d19bc60f53b3e997a3e067dea5f21a35d/poetry-1.4.0.tar.gz
+pip install https://files.pythonhosted.org/packages/e5/4f/21df008b7c3c2624f9b706f3deeab5d3f7a23d7ff364114eb693cdeaa5a5/deep-translator-1.9.2.tar.gz
+pip install https://github.com/tpaviot/pythonocc-utils/archive/refs/heads/master.zip
+pip install https://files.pythonhosted.org/packages/4e/40/9b56ca8d29453d0589a50b25184ca9d5c2d3de12af28e57e3efc02188a47/string_grouper-0.6.1-py3-none-any.whl
+IF %ERRORLEVEL% NEQ 0 exit 1
