0d4c91930259030dc31690821838cf8c89e65c1a
Sven-Heiko Hinrichs <sven.hinrichs@rwth-aachen.de>
2023-03-25 14:36:19 +0100

    ci_build_plugin_dev

--- conda_recipe/plugins/PluginEnergyPlus/build.sh
+++ conda_recipe/plugins/PluginEnergyPlus/build.sh
@@ -3,3 +3,4 @@ python -m pip install --no-deps --ignore-installed bim2sim/plugins/PluginEnergyP
 pip install https://files.pythonhosted.org/packages/07/ec/908ba180d78c2a0ba170879188877b7f5df75cae0fd2c0f090d7cd2b894c/geomeppy-0.11.8.tar.gz
 pip install https://files.pythonhosted.org/packages/db/1f/94778fa817b016da933485c35bcd401b5766f0e3703b47e225667c411146/eppy-0.5.63.tar.gz
 pip install https://github.com/NREL/EnergyPlusRegressionTool/archive/refs/tags/v1.9.6.zip
+pip install https://files.pythonhosted.org/packages/f1/97/67267678b3d088d36b09d1995c08351bfbeb8de721796340d72a92a0d8c2/poly2tri-0.3.3.tar.gz
