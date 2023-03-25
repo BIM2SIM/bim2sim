02e5502c14e2714f703878221d4432b8cf193f99
Sven-Heiko Hinrichs <sven.hinrichs@rwth-aachen.de>
2023-03-25 01:26:24 +0100

    ci_build_dev

--- conda_recipe/plugins/PluginTeaser/test_teaser.sh
+++ conda_recipe/plugins/PluginTeaser/test_teaser.sh
@@ -6,7 +6,8 @@ apt-get install unzip wget -y &&
 pip install coverage &&
 pip install coverage-badge &&
 wget --quiet -O test/TestModels/download.zip https://rwth-aachen.sciebo.de/s/SAUQQgvwqeS96ix/download &&
-unzip test/TestModels/download.zip -d test/TestModels/ &&
+unzip -o test/TestModels/download.zip -d test/TestModels/ &&
 coverage run -m unittest discover bim2sim/plugins/PluginTEASER/test/integration_test ||
 coverage report -i  ||
-coverage html -i
+coverage html -i ||
+
--- conda_recipe/plugins/PluginTeaser/test_teaser_regression.sh
+++ conda_recipe/plugins/PluginTeaser/test_teaser_regression.sh
@@ -6,10 +6,10 @@ apt-get install unzip wget -y &&
 pip install coverage &&
 pip install coverage-badge &&
 wget --quiet -O test/TestModels/download.zip https://rwth-aachen.sciebo.de/s/SAUQQgvwqeS96ix/download &&
-unzip test/TestModels/download.zip -d test/TestModels/ &&
+unzip -o test/TestModels/download.zip -d test/TestModels/ &&
 wget --quiet -O bim2sim/assets/download.zip https://rwth-aachen.sciebo.de/s/5EQqe5g8x0x4lae/download &&
-unzip bim2sim/assets/download.zip -d bim2sim/assets/ &&
+unzip -o bim2sim/assets/download.zip -d bim2sim/assets/ &&
 git clone --branch development https://github.com/RWTH-EBC/AixLib.git AixLib &&
-xvfb-run -n 77 coverage run -m unittest discover bim2sim/plugins/PluginTEASER/test/regression_test &&
-coverage report -i &&
+xvfb-run -n 77 coverage run -m unittest discover bim2sim/plugins/PluginTEASER/test/regression_test ||
+coverage report -i ||
 coverage html -i
