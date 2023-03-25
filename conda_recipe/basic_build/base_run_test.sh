ddd90d2cf3b60f80dfbcafe1d67e413ffcc75b78
Sven-Heiko Hinrichs <sven.hinrichs@rwth-aachen.de>
2023-03-25 12:16:42 +0100

    ci_build_plugin_dev

--- conda_recipe/basic_build/base_run_test.sh
+++ conda_recipe/basic_build/base_run_test.sh
@@ -6,7 +6,7 @@ apt-get install unzip wget -y &&
 pip install coverage &&
 pip install coverage-badge &&
 wget --quiet  -O test/TestModels/download.zip https://rwth-aachen.sciebo.de/s/R6K1H5Z9fiB3EoB/download &&
-unzip test/TestModels/download.zip -d test/TestModels/ &&
+unzip -o test/TestModels/download.zip -d test/TestModels/ &&
 coverage run -m unittest discover test &&
 coverage report -i  &&
 coverage html -i
--- conda_recipe/plugins/PluginLCA/test_lca.sh
+++ conda_recipe/plugins/PluginLCA/test_lca.sh
@@ -6,7 +6,7 @@ apt-get install unzip wget -y &&
 pip install coverage &&
 pip install coverage-badge &&
 wget --quiet -O test/TestModels/download.zip https://rwth-aachen.sciebo.de/s/SAUQQgvwqeS96ix/download &&
-unzip test/TestModels/download.zip -d test/TestModels/ &&
+unzip -o test/TestModels/download.zip -d test/TestModels/ &&
 coverage run -m unittest discover bim2sim/plugins/PluginLCA/test/integration_test ||
 coverage report -i ||
 coverage html -i
