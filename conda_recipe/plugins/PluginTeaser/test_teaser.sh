6f238fa7ebf8afc7deb3025bdc3dded77b43873a
Sven-Heiko Hinrichs <sven.hinrichs@rwth-aachen.de>
2023-03-25 04:05:02 +0100

    ci_build_plugin_dev

--- conda_recipe/plugins/PluginTeaser/test_teaser.sh
+++ conda_recipe/plugins/PluginTeaser/test_teaser.sh
@@ -9,5 +9,5 @@ wget --quiet -O test/TestModels/download.zip https://rwth-aachen.sciebo.de/s/SAU
 unzip -o test/TestModels/download.zip -d test/TestModels/ &&
 coverage run -m unittest discover bim2sim/plugins/PluginTEASER/test/integration_test ||
 coverage report -i  ||
-coverage html -i ||
+coverage html -i
 
