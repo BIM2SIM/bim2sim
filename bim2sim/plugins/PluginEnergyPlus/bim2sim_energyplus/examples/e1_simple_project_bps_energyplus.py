38616237bf515cdbba9fffd1f35634cc06f01ac4
veronika_richter <veronika.richter@rwth-aachen.de>
2023-07-05 17:41:16 +0200

    Update example for PluginEnergyPlus

--- bim2sim/plugins/PluginEnergyPlus/bim2sim_energyplus/examples/e1_simple_project_bps_energyplus.py
+++ bim2sim/plugins/PluginEnergyPlus/bim2sim_energyplus/examples/e1_simple_project_bps_energyplus.py
@@ -1,19 +1,20 @@
 import tempfile
 from pathlib import Path
 
+import bim2sim
 from bim2sim import Project, run_project, ConsoleDecisionHandler
 from bim2sim.kernel.log import default_logging_setup
 from bim2sim.utilities.types import IFCDomain
 
 
-def run_example_3():
+def run_example_1():
     """Run a building performance simulation with the EnergyPlus backend.
 
     This example runs a BPS with the EnergyPlus backend. Specifies project
     directory and location of the IFC file. Then, it creates a bim2sim
-    project with the EnergyPlus backend. Workflow settings are specified (here,
-    the zoning setup is specified to be with a full level of detail: each
-    IfcSpace is represented by a single thermal Zone in EnergyPlus),
+    project with the EnergyPlus backend. Simulation settings are specified
+    (EnergyPlus location needs to be specified according to your system,
+    other settings are set to default if not specified otherwise),
     before the project is executed with the previously specified settings.
     """
     # Create the default logging to for quality log and bim2sim main log (
@@ -22,18 +23,23 @@ def run_example_3():
 
     # Create a temp directory for the project, feel free to use a "normal"
     # directory
-    project_path = Path(tempfile.TemporaryDirectory(
-        prefix='bim2sim_example3').name)
+    project_path = Path(
+        tempfile.TemporaryDirectory(prefix='bim2sim_example1').name)
 
     # Get path of the IFC Building model that is used for this example
-    ifc_paths = {
-        IFCDomain.arch: Path(__file__).parent.parent
-                        / 'assets/ifc_example_files/AC20-FZK-Haus.ifc',
-    }
+    ifc_paths = {IFCDomain.arch: Path(
+        bim2sim.__file__).parent /
+                                 'assets/ifc_example_files/AC20-FZK-Haus.ifc',
+                 }
     # Create a project including the folder structure for the project with
-    # energyplus as backend and no specified workflow
-    # (default workflow is taken)
+    # energyplus as backend
     project = Project.create(project_path, ifc_paths, 'energyplus')
+
+    # Set the install path to your EnergyPlus installation according to your
+    # system requirements
+    # project.sim_settings.ep_install_path = 'C://EnergyPlusV9-4-0/'
+
+    # Set other simulation settings, otherwise all settings are set to default
 
     # Run the project with the ConsoleDecisionHandler. This allows interactive
     # input to answer upcoming questions regarding the imported IFC.
@@ -41,4 +47,4 @@ def run_example_3():
 
 
 if __name__ == '__main__':
-    run_example_3()
+    run_example_1()
