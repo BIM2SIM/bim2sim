c637733955c5af3a38d10b40e7a1bcd68ab9672a
David <david.jansen1@rwth-aachen.de>
2023-07-25 18:05:28 +0200

    import tz for example

--- bim2sim/plugins/PluginTEASER/bim2sim_teaser/examples/e1_simple_project_bps_teaser.py
+++ bim2sim/plugins/PluginTEASER/bim2sim_teaser/examples/e1_simple_project_bps_teaser.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import bim2sim
 from bim2sim import Project, run_project, ConsoleDecisionHandler
+from bim2sim.elements.bps_elements import ThermalZone
 from bim2sim.kernel.log import default_logging_setup
 from bim2sim.utilities.types import IFCDomain, LOD, ZoningCriteria
 
