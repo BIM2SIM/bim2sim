be585f58cb51e6a279861bd557b4f8c9f3b0c181
veronika_richter <veronika.richter@rwth-aachen.de>
2023-09-29 17:57:57 +0200

    Run EnergyPlus as subprocess #bugfix #hotfix as EPPY does not produce csv output (known EPPY issue: https://github.com/santoshphilip/eppy/issues/218)

--- bim2sim/plugins/PluginEnergyPlus/bim2sim_energyplus/task/ep_run_simulation.py
+++ bim2sim/plugins/PluginEnergyPlus/bim2sim_energyplus/task/ep_run_simulation.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from bim2sim.tasks.base import ITask
 
 
@@ -11,8 +13,24 @@ class RunEnergyPlusSimulation(ITask):
         if not ep_full:
             design_day = True
         output_string = str(self.paths.export / 'EP-results/')
-        idf.run(output_directory=output_string, readvars=True, annual=ep_full,
-                design_day=design_day)
+        # idf.run(output_directory=output_string, readvars=True,
+        # annual=ep_full,
+        #        design_day=design_day)
+        run_options = []
+        run_options.extend([self.playground.sim_settings.ep_install_path/
+                            'energyplus.exe', '-w',
+                            self.playground.sim_settings.overwrite_weather,
+                            '-r', '-x', '-d', output_string])
+        if not ep_full:
+            run_options.extend(['-D'])
+        else:
+            run_options.extend(['-a'])
+        subprocess.run([*run_options, idf.idfname])
+        # subprocess.run([self.playground.sim_settings.ep_install_path/
+        #                 'energyplus.exe',
+        #                '-w',
+        #                 self.playground.sim_settings.overwrite_weather,
+        #                 '-r', '-x', '-d', output_string, idf.idfname])
         self.playground.sim_settings.simulated = True
         self.logger.info(f"Simulation successfully finished.")
         # if ep_full:
