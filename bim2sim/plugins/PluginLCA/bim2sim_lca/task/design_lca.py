090f40f7891fd9602b13ac797429f9e2fc5f3a40
Sven Hartmann <sven.hartmann@eonerc.rwth-aachen.de>
2023-11-26 01:08:00 +0100

    Steinerbaum optimiert in 3D

--- bim2sim/plugins/PluginLCA/bim2sim_lca/task/design_lca.py
+++ bim2sim/plugins/PluginLCA/bim2sim_lca/task/design_lca.py
@@ -24,9 +24,12 @@ class DesignLCA(ITask):
 
     a = "test"
 
+
+
     def run(self, instances):
 
         visualisierung = True
+        starting_point = [50, 0, 0]
 
         self.logger.info("Start design LCA")
         thermal_zones = filter_instances(instances, 'ThermalZone')
@@ -36,7 +39,7 @@ class DesignLCA(ITask):
         # Hier werden die Mittelpunkte der einzelnen Räume aus dem IFC-Modell ausgelesen und im Anschluss um die
         # halbe Höhe des Raumes nach oben verschoben. Es wird also der Punkt an der UKRD (Unterkante Rohdecke)
         # in der Mitte des Raumes berechnet. Hier soll im weiteren Verlauf der Lüftungsauslass angeordnet werden
-        center = self.center(thermal_zones)
+        center = self.center(thermal_zones, starting_point)
         self.logger.info("Finished calculating points of the ventilation outlet at the ceiling")
 
         self.logger.info("Getting Airflow Data")
@@ -96,7 +99,7 @@ class DesignLCA(ITask):
         rundungsregel = Decimal('1').scaleb(-stellen)  # Gibt die Anzahl der Dezimalstellen an
         return float(zahl_decimal.quantize(rundungsregel, rounding=ROUND_HALF_UP))
 
-    def center(self, thermal_zones):
+    def center(self, thermal_zones, starting_point):
         """Function calculates position of the outlet of the LVA
 
         Args:
@@ -202,6 +205,27 @@ class DesignLCA(ITask):
                 i = j
 
         room_ceiling_ventilation_outlet = adjusted_coords_y
+        print(room_ceiling_ventilation_outlet)
+
+        # Hier werden die Startpunkte (Schachtauslässe) je Ebene hinzugefügt und die gesamte Luftmenge an für die Ebene
+        # berechnet. Diese wird für den Graphen gebraucht
+
+        airflow_volume_per_storey = {}
+
+        for sublist in room_ceiling_ventilation_outlet:
+            z = sublist[2]  # Der dritte Eintrag (Index 2) ist 'z'
+            a = sublist[3]  # Der vierte Eintrag (Index 3) ist 'a'
+            if z in airflow_volume_per_storey:
+                airflow_volume_per_storey[z] += a
+            else:
+                airflow_volume_per_storey[z] = a
+        print(airflow_volume_per_storey)
+
+        for z_coord in z_axis:
+            room_ceiling_ventilation_outlet.append((starting_point[0],starting_point[1],z_coord,
+                                                    airflow_volume_per_storey[z_coord]))
+
+        print(room_ceiling_ventilation_outlet)
 
         return room_ceiling_ventilation_outlet
 
@@ -248,6 +272,8 @@ class DesignLCA(ITask):
         # die Schnittpunkte, welche ein Lüftungsauslass sind
 
         return intersection_points_list
+
+    # def grid_points(self):
 
     def visualisierung(self, room_ceiling_ventilation_outlet, air_flow_building, intersection):
         """The function visualizes the points in a diagram
@@ -430,7 +456,12 @@ class DesignLCA(ITask):
 
 
 
+        # Hier wird ein leerer Graph erstellt. Dieser wird im weiteren Verlauf mit den Graphen der einzelnen Ebenen
+        # angereichert
+        three_dimensional_graph = nx.Graph()
+
         for z_value in z_coordinate_set:
+            # Hier wird für jede Ebene ein einzelner Steinergraph erstellt.
             filtered_coords_ceiling_weight = [coord for coord in ceiling_point if coord[2] == z_value]
             filtered_coords_intersection = [coord for coord in intersection_points if coord[2] == z_value]
 
@@ -445,23 +476,14 @@ class DesignLCA(ITask):
             # unbedingt miteinander verbunden werden müssen
             terminals = list()
 
-            # Hinzufügen der Knoten für Lüftungsauslässe
+            # Hinzufügen der Knoten für Lüftungsauslässe zu Terminals
             for x, y, z, a in filtered_coords_ceiling_weight:
                 G.add_node((x, y, z), weight=int(a))
                 if int(a) > 0:  # Bedingung, um Terminals zu bestimmen (z.B. Gewicht > 0)
                     terminals.append((x, y, z))
 
 
-            # Steinerpunkte
-            # Steinerpunkte sind zusätzliche Punkte im Graphen, die nicht zu den urspünglichen Terminals gehören. Sie
-            # können in die Lösung des Steinerbaum-Problems aufgenommen werden, um die Gesamtlänge des Baumes
-            # zu minimieren
-            steiners = list()
-
             coordinates = filtered_coords_ceiling + filtered_coords_intersection
-            for x, y, z in filtered_coords_intersection:
-                G.add_node((x, y, z), weight=0)
-                steiners.append((x, y, z))
 
 
             # Kanten entlang der X-Achse hinzufügen
@@ -485,39 +507,71 @@ class DesignLCA(ITask):
 
 
             # Steinerbaum
+            steiner_baum = steiner_tree(G, terminals, weight="weight")
+            print(steiner_baum)
 
-            baum = steiner_tree(G, terminals, weight="weight")
-            print(baum)
-
+            # Nachden der Graph für die einzelne Ebene erstellt wurde, wird dieser hier zum 3D Graphen hinzugefügt
+            three_dimensional_graph = nx.union(three_dimensional_graph, steiner_baum)
 
             if visualisierung == True:
                 # Visualisierung
-                plt.figure(figsize=(15, 10))
+                plt.figure()
                 plt.xlabel('X-Achse [m]')
                 plt.ylabel('Y-Achse [m]')
                 plt.title(f"Graph mit Kantengewichten und Knotengewichten, Z: {z_value}")
                 plt.grid(False)
-                plt.subplots_adjust(right=0.7)
+                # plt.subplots_adjust(right=0.7)
 
                 # Positionen der Knoten festlegen
                 pos = {coord: (coord[0], coord[1]) for coord in coordinates}
 
                 # Knoten zeichnen
-                nx.draw_networkx_nodes(baum, pos, nodelist=filtered_coords_ceiling, node_shape='D', node_color='blue',
+                nx.draw_networkx_nodes(steiner_baum, pos, nodelist=filtered_coords_ceiling, node_shape='D', node_color='blue',
                                        node_size=250)
-                nx.draw_networkx_nodes(baum, pos, nodelist=filtered_coords_intersection, node_shape='o', node_color='red',
+                nx.draw_networkx_nodes(steiner_baum, pos, nodelist=filtered_coords_intersection, node_shape='o', node_color='red',
                                        node_size=100)
 
                 # Kanten zeichnen
-                nx.draw_networkx_edges(baum, pos)
+                nx.draw_networkx_edges(G, pos, width=1)
+                nx.draw_networkx_edges(steiner_baum, pos, width=4, style="-.", edge_color="green")
 
                 # Kantengewichte anzeigen
-                edge_labels = nx.get_edge_attributes(baum, 'weight')
-                nx.draw_networkx_edge_labels(baum, pos, edge_labels=edge_labels, font_size=5, font_weight=5)
+                edge_labels = nx.get_edge_attributes(steiner_baum, 'weight')
+                nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=10, font_weight=10)
+                # nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=5, font_weight=10)
 
                 # Knotengewichte anzeigen
-                node_labels = nx.get_node_attributes(baum, 'weight')
-                nx.draw_networkx_labels(baum, pos, labels=node_labels, font_size=8, font_color="white")
+                node_labels = nx.get_node_attributes(steiner_baum, 'weight')
+                nx.draw_networkx_labels(steiner_baum, pos, labels=node_labels, font_size=8, font_color="white")
 
                 # Anzeigen des Graphens
                 plt.show()
+
+        print(three_dimensional_graph)
+
+        # Darstellung des 3D-Graphens:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        # Knotenpositionen in 3D
+        pos = {coord: (coord[0], coord[1], coord[2]) for coord in list(three_dimensional_graph.nodes())}
+
+        # Knoten zeichnen
+        for node, (x, y, z) in pos.items():
+            ax.scatter(x, y, z, color='blue', s=100)
+
+        # Kanten zeichnen
+        for edge in three_dimensional_graph.edges:
+            x0, y0, z0 = pos[edge[0]]
+            x1, y1, z1 = pos[edge[1]]
+            ax.plot([x0, x1], [y0, y1], [z0, z1], color='black')
+
+        # Achsenbeschriftungen und Titel
+        ax.set_xlabel('X-Achse [m]')
+        ax.set_ylabel('Y-Achse [m]')
+        ax.set_zlabel('Z-Achse [m]')
+        ax.set_title("3D Graph mit Kantengewichten und Knotengewichten")
+
+        # Diagramm anzeigen
+        plt.show()
+
