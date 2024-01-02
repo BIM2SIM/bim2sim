9921df3791065dd353df10be4de8d97d605c7ff2
David Jansen <david.jansen1@rwth-aachen.de>
2024-01-02 11:46:28 +0100

    adjust static template for spawn

--- bim2sim/plugins/PluginSpawn/bim2sim_spawn/tasks/export_co_sim_static.py
+++ bim2sim/plugins/PluginSpawn/bim2sim_spawn/tasks/export_co_sim_static.py
@@ -37,12 +37,13 @@ class ExportModelicaSpawnStatic(ITask):
         # TODO multithreading lock needed? see modelica/__init__.py for example
         # with lock:
         data = template.render(
-            model_name=self.to_modelica('building_simulation'),
-            model_comment=self.to_modelica('test2'),
-            weather_path_ep=self.to_modelica(weather_path_ep),
-            weather_path_mos=self.to_modelica(weather_path_mos),
-            zone_names=self.to_modelica(zone_names),
-            idf_path=self.to_modelica(idf_path)
+            model_name='building_simulation',
+            model_comment='test2',
+            weather_path_ep=self.to_modelica_spawn(weather_path_ep),
+            weather_path_mos=self.to_modelica_spawn(weather_path_mos),
+            zone_names=self.to_modelica_spawn(zone_names),
+            idf_path=self.to_modelica_spawn(idf_path),
+            n_zones=len(zone_names)
         )
 
 
@@ -70,7 +71,7 @@ class ExportModelicaSpawnStatic(ITask):
         return zone_list
 
     @staticmethod
-    def to_modelica(parameter):
+    def to_modelica_spawn(parameter):
         """converts parameter to modelica readable string"""
         if parameter is None:
             return parameter
@@ -79,10 +80,10 @@ class ExportModelicaSpawnStatic(ITask):
         if isinstance(parameter, (int, float)):
             return str(parameter)
         if isinstance(parameter, str):
-            return '%s' % parameter
+            return '"%s"' % parameter
         if isinstance(parameter, (list, tuple, set)):
             return "{%s}" % (
-                ",".join((ExportModelicaSpawnStatic.to_modelica(par) for par in parameter)))
+                ",".join((ExportModelicaSpawnStatic.to_modelica_spawn(par) for par in parameter)))
         if isinstance(parameter, Path):
             return \
                 f"Modelica.Utilities.Files.loadResource(\"{str(parameter)}\")"\
