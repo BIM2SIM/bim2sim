662bdef8fc1136024ba8383875c7aea337180352
hochbean <anna.hochberger@rwth-aachen.de>
2024-03-13 13:05:20 +0100

    Add tasks for running OpenFOAM meshing and simulation

--- bim2sim/plugins/PluginOpenFOAM/bim2sim_openfoam/task/run_meshing.py
+++ bim2sim/plugins/PluginOpenFOAM/bim2sim_openfoam/task/run_meshing.py
@@ -1,18 +1,77 @@
 from bim2sim.tasks.base import ITask
+import sys
+import os
+from butterfly.butterfly import decomposeParDict as decPD
 
 
 class RunOpenFOAMMeshing(ITask):
-    """This ITask runs the openfoam meshing.
+    """This ITask runs the openfoam meshing on linux systems.
     """
 
-    reads = ('openfoam_case',)
+    reads = ()
     touches = ()
 
     def __init__(self, playground):
         super().__init__(playground)
+        self.base_path = self.paths + '/OpenFOAM'
 
-    def run(self, openfoam_case):
-        # todo: add RunOpenFOAM meshing, which should only be executed on
-        #  linux (and check for #cores before run and match with
-        #  decomposeParDict)
-        pass
+    def run(self):
+        if not sys.platform == 'linux':
+            print('Execution on non-Linux systems is not recommended.')
+            return
+        run = input(
+            "This will take several minutes. Proceed anyways? [Y/n] \n")
+        if run == 'Y':
+            # Use half of the available processes
+            procs = os.cpu_count()
+            procs = round(procs / 4) * 2
+            distrib = self.split_into_three_factors(procs)
+
+            # Write updated distribution to decomposeParDict
+            dPpath = self.base_path + 'system/decomposeParDict'
+            decomposeParDict = decPD.DecomposeParDict()
+            decomposeParDict = decomposeParDict.from_file(dPpath)
+            decomposeParDict.set_value_by_parameter('numberOfSubdomains',
+                                                    str(procs))
+            hc1 = decomposeParDict.get_value_by_parameter('hierarchicalCoeffs')
+            distrib = '(' + str(distrib[0]) + ' ' + str(
+                distrib[1]) + ' ' + str(
+                distrib[2]) + ')'
+            hc1['n'] = distrib
+            decomposeParDict.set_value_by_parameter('hierarchicalCoeffs', hc1)
+            decomposeParDict.save(project_folder=self.base_path)
+
+            # execution
+            cwd = os.getcwd()
+            os.chdir(self.base_path)
+            os.system('pwd')
+            os.system('blockMesh')
+            os.system('decomposePar -force')
+            print('Writing snappyHexMesh output to file \'logMeshing\'.')
+            os.system('mpiexec -np ' + str(procs) + ' snappyHexMesh -parallel '
+                                                    '-overwrite > logMeshing')
+            print('Done meshing.')
+            os.system('reconstructParMesh -mergeTol 1e-10 -constant')
+            os.system('decomposePar -cellDist -dry-run')
+            os.chdir(cwd)
+
+    @staticmethod
+    def prime_factors(n):
+        factors = []
+        divisor = 2
+        while n > 1:
+            while n % divisor == 0:
+                factors.append(divisor)
+                n //= divisor
+            divisor += 1
+        return factors
+
+    def split_into_three_factors(self, n):
+        factors = self.prime_factors(n)
+        factors.sort()
+        groups = []
+        for i, factor in enumerate(factors):
+            groups.append(factor)
+        if len(groups) < 3:
+            groups.append(1)
+        return groups
--- bim2sim/plugins/PluginOpenFOAM/bim2sim_openfoam/task/run_openfoam_simulation.py
+++ bim2sim/plugins/PluginOpenFOAM/bim2sim_openfoam/task/run_openfoam_simulation.py
@@ -1,18 +1,43 @@
 from bim2sim.tasks.base import ITask
+import sys
+import os
 
 
 class RunOpenFOAMSimulation(ITask):
-    """This ITask runs the openfoam simulation.
+    """This ITask runs the openfoam simulation on linux systems.
     """
 
-    reads = ('openfoam_case',)
+    reads = ()
     touches = ()
 
     def __init__(self, playground):
         super().__init__(playground)
+        self.base_path = self.paths + '/OpenFOAM'
 
-    def run(self, openfoam_case):
-        # todo: add RunOpenFOAM simulation, which should only be executed on
-        #  linux (and check for #cores before run and match with
-        #  decomposeParDict)
-        pass
+    def run(self):
+        if not sys.platform == 'linux':
+            print('Execution on non-Linux systems is not recommended.')
+            return
+        run = input(
+            "Warning: Meshing must be done in advance. \n This will take "
+            "a while. Proceed anyways? [Y/n] \n")
+        if run == 'Y':
+            # Use half of the available processes
+            procs = os.cpu_count()
+            procs = round(procs / 4) * 2
+
+            # Execution
+            cwd = os.getcwd()
+            os.chdir(self.base_path)
+            os.system('pwd')
+            os.system('topoSet')
+            os.system('setsToZones')
+            os.system('decomposePar -force')
+            print(
+                'Writing buoyantSimpleFoam output to file \'logSimulation\'.')
+            os.system('mpiexec -np ' + str(procs) + ' buoyantSimpleFoam '
+                                                    '-parallel > logSimulation')
+            print('Done simulating.')
+            os.system('reconstructPar -latestTime')
+            os.chdir(cwd)
+        
