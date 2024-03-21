07f6016598b19237771f5aeeab4bbacabd8b0e06
falk.cudok <falk.cudok@tu-berlin.de>
2024-03-21 14:28:56 +0100

    simple version of generate_diagram
    more featured example

--- plugindiagram/testtemplatemermaid.py
+++ plugindiagram/testtemplatemermaid.py
@@ -1,40 +1,58 @@
 """Testing to generate mermaid code via templetes/template-functions."""
 
 
-def task_code(taskname: str = "bim2sim task"):
+def task_code(taskname: str = "bim2simtask",
+              task_belongs: str = "belongsBim2sim"):
     """Generate mermaid code representing a bim2sim task.
 
     WIP: so the some structure stuff like tpye of diagram is added here, later
     it should move to def generate_diagram".
 
     Args:
-      taskname: name of the bim2sim taks of the plugin
-      task_belongs: submodul the task belongs to of the bim2sim project
+      taskname: name of the bim2sim taks of the plugin, no space allowed
+      task_belongs: submodul the task belongs to of the bim2sim project,
+                    no space allowed
 
     Returns:
       Mermaid code of an task.
       WIP: check how to pipe. now it will printed
+
+    Attention:
+      - Tasknames should be unique, when merge different mermaid
+        templates instances.
+
     """
     code_template = """
-    flowchart TB
-    subgraph "task {taskname}"
-     tli["common.{taskname}"]
-     subgraph reads & touches
-      direction LR
-      rli[/"None"/]
-      toli[\"ifc_files"\]
-     end
-     extli("reads the IFC files of one or multiple domains inside bim2sim")
-     end
+subgraph "task {taskname}"
+t{taskname}["{task_belongs}.{taskname}"]
+subgraph reads & touches
+ direction LR
+ r{taskname}[/"None"/]
+ to{taskname}[\"ifc_files"\]
+end
+ext{taskname}("reads the IFC files of one or multiple domains inside bim2sim")
+end
     """
-    code = code_template.format(taskname=taskname)
+    code = code_template.format(taskname=taskname, task_belongs=task_belongs)
 
     return code
 
 
-def generate_diagram():
-    """Print mermaid code of the whole task structure of one bim2sim plugin."""
-    pass
+def generate_diagram(tasks: list):
+    """Print mermaid code of the whole task structure of one bim2sim plugin.
+
+    The plugin structure is fix: next plugin is connected to the plugin before.
+
+    Args:
+      tasks: list of tasks (mermaid code strings)
+    """
+    digram_type = "flowchart TB"
+
+    mermaid_code = digram_type
+    for task_code in tasks:
+        mermaid_code = mermaid_code + task_code
+
+    return mermaid_code
 
 
 def write_file(mermaid_code: str, filename: str):
@@ -46,4 +64,8 @@ def write_file(mermaid_code: str, filename: str):
 if __name__ == '__main__':
     # Examples
     print(task_code())
-    write_file(task_code("LooadIFC"), "test_template_code.mmd")
+    write_file(generate_diagram([task_code("LooadIFC"),
+                                 task_code("LooadIFC1"),
+                                 task_code("LooadIFC5")
+                                ]),
+               "test_template_code.mmd")
