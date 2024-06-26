d3c97e49e9d4b19bf09befb302dbbefd3cd0ae8e
falk.cudok <falk.cudok@tu-berlin.de>
2024-06-26 13:24:17 +0200

    add content from development branch

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,13 +3,15 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'bim2sim'
-copyright = '2024, bim'
-author = 'bim'
-release = '0.1'
+# -- Project information -----------------------------------------------------
+
+project = 'bim2sim '
+copyright = '2022, RWTH Aachen University, EBC & E3D; ROM Technik GmbH'
+# author = 'David Jansen (todo)'
+
+# The full version, including alpha/beta/rc tags
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.ml#general-configuration
@@ -26,5 +28,17 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
 html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = "img/static/b2s_logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
