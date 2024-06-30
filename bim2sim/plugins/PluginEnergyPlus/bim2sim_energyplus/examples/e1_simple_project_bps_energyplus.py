24396ee68ec4e45329f5062839f8758175fa6ead
veronikarichter <veronika.richter@rwth-aachen.de>
2024-06-30 18:26:52 +0200

    remove ep_install_path

--- bim2sim/plugins/PluginEnergyPlus/bim2sim_energyplus/examples/e1_simple_project_bps_energyplus.py
+++ bim2sim/plugins/PluginEnergyPlus/bim2sim_energyplus/examples/e1_simple_project_bps_energyplus.py
@@ -47,7 +47,7 @@ def run_example_1():
             'test/resources/weather_files/DEU_NW_Aachen.105010_TMYx.epw')
     # Set the install path to your EnergyPlus installation according to your
     # system requirements
-    project.sim_settings.ep_install_path = 'C://EnergyPlusV9-4-0/'
+    # project.sim_settings.ep_install_path = 'C://EnergyPlusV9-4-0/'
 
     # run annual simulation for EnergyPlus
     project.sim_settings.run_full_simulation = True
