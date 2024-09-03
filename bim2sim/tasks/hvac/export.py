daa00ebdaf68b7a35dea6fc758298f6c67fc0282
sfreund1 <sfreund@rom-technik.de>
2024-09-03 10:02:26 +0200

    Improves readability and documentation of CreateModelicaModel

--- bim2sim/tasks/hvac/export.py
+++ bim2sim/tasks/hvac/export.py
@@ -1,14 +1,17 @@
 import re
 from datetime import datetime
 from pathlib import Path
+from typing import Tuple, List, Dict
 
 from mako.template import Template
 
 from bim2sim.elements import hvac_elements as hvac
 from bim2sim.elements.base_elements import ProductBased
 from bim2sim.elements.graphs.hvac_graph import HvacGraph
+from bim2sim.elements.hvac_elements import HVACProduct
 from bim2sim.export import modelica
-from bim2sim.export.modelica import HeatTransferType, ModelicaParameter
+from bim2sim.export.modelica import HeatTransferType, ModelicaParameter, \
+    ModelicaElement
 from bim2sim.tasks.base import ITask
 from bim2sim.export.modelica import help_package, help_package_order
 
@@ -30,55 +33,69 @@ class CreateModelicaModel(ITask):
         3. Collects and exports parameters for each Modelica instance.
         4. Creates connections between Modelica instances based on the HVAC
         graph.
-        5. Creates a Modelica model with the exported elements and connections.
-        6. Saves the Modelica model to the specified export path.
+        5. Creates heat port connections (both inner and outer).
+        6. Returning the created elements and connections.
 
         Args:
             libraries: Tuple of libraries to be used in Modelica.
             graph: The HVAC graph to be exported.
+
+        Returns:
+            export_elements: A mapping of HVAC elements to their corresponding
+                Modelica instances.
+            connections: A list of connections between the Modelica instances.
+            cons_heat_ports_conv: A list of convective heat port connections.
+            cons_heat_ports_rad: A list of radiative heat port connections.
         """
-
-        self.logger.info("Export to Modelica code")
-        elements = graph.elements
-        # sort elements
-        elements = sorted(elements, key=lambda x: x.guid)
-
+        # Initialize Modelica factory and create Modelica instances
+        self.logger.info("Exporting HVAC graph to Modelica code")
+        elements = sorted(graph.elements, key=lambda x: x.guid)
         modelica.ModelicaElement.init_factory(libraries)
-        export_elements = {inst: modelica.ModelicaElement.factory(inst)
-                           for inst in elements}
-        connections = self.create_connections(graph, export_elements)
+        export_elements = self._create_export_elements(elements)
 
-        # Perform decisions for requested but not existing attributes
-        yield from ProductBased.get_pending_attribute_decisions(elements)
-        # Perform decisions for required but not existing modelica parameters
-        yield from ModelicaParameter.get_pending_parameter_decisions()
+        # Create connections based on HVAC graph
+        connections = self._create_connections(graph, export_elements)
 
-        # All parameters are checked against the specified check function and
-        #  exported with the correct unit
-        for instance in export_elements.values():
-            instance.collect_params()
+        # Handle pending attribute and parameter decisions
+        yield from self._handle_pending_decisions(elements)
 
-        inner_heat_port_cons_conv, inner_heat_port_cons_rad = (
-            self.create_inner_heat_port_connections())
+        # Collect parameters for each Modelica instance
+        self._collect_parameters(export_elements)
 
-        if self.playground.sim_settings.outer_heat_ports:
-            outer_heat_port_cons_conv, outer_heat_port_cons_rad = (
-                self.create_outer_heat_port_connections(
-                    list(export_elements.values())))
-        else:
-            outer_heat_port_cons_conv = []
-            outer_heat_port_cons_rad = []
-        cons_heat_ports_conv = (outer_heat_port_cons_conv +
-                                inner_heat_port_cons_conv)
-        cons_heat_ports_rad = (outer_heat_port_cons_rad +
-                               inner_heat_port_cons_rad)
+        # Create heat port connections
+        cons_heat_ports_conv, cons_heat_ports_rad = (
+            self._create_heat_port_connections(export_elements))
 
-        # TODO #1 integrate heat ports in connections
+        # # TODO #1 integrate heat ports in connections
+        # connections.extend(cons_heat_ports_conv)
+        # connections.extend(cons_heat_ports_rad)
         return (export_elements, connections,
                 cons_heat_ports_conv, cons_heat_ports_rad)
 
     @staticmethod
-    def create_connections(graph: HvacGraph, export_elements: dict) -> list:
+    def _create_export_elements(elements: List[HVACProduct]
+                                ) -> Dict[HVACProduct, ModelicaElement]:
+        """Create Modelica instances for each HVAC element."""
+        return {inst: modelica.ModelicaElement.factory(inst)
+                for inst in elements}
+
+    @staticmethod
+    def _handle_pending_decisions(elements: List[HVACProduct]):
+        """Handle pending attribute and parameter decisions."""
+        yield from ProductBased.get_pending_attribute_decisions(elements)
+        yield from ModelicaParameter.get_pending_parameter_decisions()
+
+    @staticmethod
+    def _collect_parameters(
+            export_elements: Dict[HVACProduct, ModelicaElement]):
+        """Collect and export parameters for each Modelica instance."""
+        for instance in export_elements.values():
+            instance.collect_params()
+
+    @staticmethod
+    def _create_connections(graph: HvacGraph,
+                            export_elements: Dict[HVACProduct, ModelicaElement]
+                            ) -> List[Tuple[str, str]]:
         """Creates a list of connections for the corresponding Modelica model.
 
         This method iterates over the edges of the HVAC graph and creates a
@@ -90,8 +107,8 @@ class CreateModelicaModel(ITask):
             export_elements: the Modelica elements
 
         Returns:
-           connection_port_names: list of tuple of port names that are
-           connected.
+            connection_port_names: list of tuple of port names that are
+                connected.
         """
         connection_port_names = []
         distributors_n = {}
@@ -127,10 +144,40 @@ class CreateModelicaModel(ITask):
 
         return connection_port_names
 
+    def _create_heat_port_connections(
+            self, export_elements: Dict[HVACProduct, ModelicaElement]
+    ) -> Tuple[List[Tuple[str, str]], List[Tuple[str, str]]]:
+        """Create inner and outer heat port connections."""
+        inner_heat_port_cons_conv, inner_heat_port_cons_rad = (
+            self.create_inner_heat_port_connections())
+
+        if self.playground.sim_settings.outer_heat_ports:
+            outer_heat_port_cons_conv, outer_heat_port_cons_rad = (
+                self.create_outer_heat_port_connections(
+                    list(export_elements.values())))
+        else:
+            outer_heat_port_cons_conv, outer_heat_port_cons_rad = [], []
+
+        cons_heat_ports_conv = (outer_heat_port_cons_conv +
+                                inner_heat_port_cons_conv)
+        cons_heat_ports_rad = (outer_heat_port_cons_rad +
+                               inner_heat_port_cons_rad)
+
+        return cons_heat_ports_conv, cons_heat_ports_rad
+
     @staticmethod
-    def create_outer_heat_port_connections(export_elements: list) -> \
-            [list, list]:
-        """Creates connections to an outer heat port for further connections"""
+    def create_outer_heat_port_connections(
+            export_elements: List[ModelicaElement]
+    ) -> Tuple[List[Tuple[str, str]], List[Tuple[str, str]]]:
+        """Creates connections to an outer heat port for further connections
+
+            Connects heat ports of the export elements to outer heat ports
+            (convective and radiative). Returns two lists containing these
+            connections.
+
+        Returns:
+            Two lists containing convective and radiative heat port connections.
+        """
         # ToDo only connect heat ports that might not be connected already by
         #  create_inner_heat_port_connections() function
 
