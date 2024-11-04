003acaaa2e6a3b895c0f6063392171f0be85ddcc
David Paul Jansen <david.jansen1@rwth-aachen.de>
2024-11-04 13:27:45 +0100

    add option to retrieve __version__ from inside code

--- bim2sim/__init__.py
+++ bim2sim/__init__.py
@@ -1,8 +1,15 @@
 """bim2sim library"""
+from importlib.metadata import version
 
 from bim2sim.kernel.decision.console import ConsoleDecisionHandler
 from bim2sim.kernel.decision.decisionhandler import DecisionHandler
 from bim2sim.project import Project
+
+
+try:
+    __version__ = version("bim2sim")
+except Exception:
+    __version__ = "unknown"
 
 
 def run_project(project: Project, handler: DecisionHandler):
