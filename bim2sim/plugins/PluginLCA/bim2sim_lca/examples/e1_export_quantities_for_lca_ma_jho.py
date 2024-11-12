4ad07ac315ec33cdeab72b1345fd52d022ce9e2f
Jonatan Höpp <jonatan.hoepp@eonerc.rwth-aachen.de>
2024-11-12 18:00:15 +0100

    Update

---
+++ bim2sim/plugins/PluginLCA/bim2sim_lca/examples/e1_export_quantities_for_lca_ma_jho.py
@@ -0,0 +1,85 @@
+import tempfile
+from pathlib import Path
+
+import bim2sim
+from bim2sim import Project, run_project, ConsoleDecisionHandler
+from bim2sim.kernel.decision.decisionhandler import DebugDecisionHandler
+from bim2sim.kernel.log import default_logging_setup
+from bim2sim.utilities.common_functions import download_test_resources
+from bim2sim.utilities.types import IFCDomain
+
+
+def run_example_complex_building_lca(project_path):
+    """Generate output for an LCA analysis.
+
+    This example generates output for an LCA analysis. Specifies project
+    directory and location of the IFC file. Then, it creates a bim2sim
+    project with the lca backend. The project is executed with the
+    previously specified settings.
+
+    After execution, go to the export folder and have a look at the two .csv
+    files. <Material_quantities_ERC_Mainbuilding_Arch.csv> will offer you
+    information about the amount (mass) of each material used in the building.
+    <Quantities_overview_ERC_Mainbuilding_Arch.csv> will give you an overview
+    about all elements separately and their materials.
+    """
+    # Create the default logging to for quality log and bim2sim main log (
+    # see logging documentation for more information
+    default_logging_setup()
+
+
+    # download additional test resources for arch domain, you might want to set
+    # force_new to True to update your test resources
+    download_test_resources(IFCDomain.arch, force_new=False)
+
+    # Get path of the IFC Building model that is used for this example
+    # In this case the mainbuilding of EBC at Aachen which has mostly correct
+    # implemented materials in IFC
+    ifc_paths = {
+        IFCDomain.arch:
+            Path(bim2sim.__file__).parent.parent /
+            'test/resources/arch/ifc/'
+            'AC20-Institute-Var-2.ifc'
+    }
+    # Create a project including the folder structure for the project with
+    # LCA as backend and no specified workflow (default workflow is taken)
+    project = Project.create(project_path, ifc_paths, 'lca')
+
+    # set weather file data
+    project.sim_settings.weather_file_path = (
+            Path(bim2sim.__file__).parent.parent /
+            'test/resources/weather_files/DEU_NW_Aachen.105010_TMYx.mos')
+
+    project.sim_settings.update_emission_parameter_from_oekobdauat = False
+    project.sim_settings.calculate_lca_building = True
+    project.sim_settings.calculate_lca_hydraulic_system = True
+    project.sim_settings.pipe_type = "Stahlrohr"
+
+    project.sim_settings.heat_delivery_type = "UFH"
+    project.sim_settings.ufh_pipe_type = "PEX"
+
+    project.sim_settings.hydraulic_system_material_xlsx = (Path(project_path, "export", "hydraulic system",
+                                                           "material_quantities_hydraulic_system.xlsx"))
+
+    answers = (2015,)
+    handler = DebugDecisionHandler(answers)
+    handler.handle(project.run())
+
+    # Run the project with the ConsoleDecisionHandler. No questions for this
+    # example will be prompted.
+    # run_project(project, ConsoleDecisionHandler())
+
+    # Go to the export folder and have a look at the two .csv files.
+    # <Material_quantities_ERC_Mainbuilding_Arch.csv> will offer you information
+    # about the amount (mass) of each material used in the building
+    # Quantities_overview_ERC_Mainbuilding_Arch.csv will give you an overview
+    # about all elements separately and their materials
+
+    total_gwp_building = project.playground.state['total_gwp_building']
+    total_gwp_hydraulic_pipe = project.playground.state['total_gwp_hydraulic_pipe']
+    total_gwp_hydraulic_component = project.playground.state['total_gwp_hydraulic_component']
+
+    return total_gwp_building, total_gwp_hydraulic_pipe, total_gwp_hydraulic_component
+
+if __name__ == '__main__':
+    run_example_complex_building_lca()
