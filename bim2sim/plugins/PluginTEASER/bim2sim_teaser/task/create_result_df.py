a04fdc7b31f893ba802b7d14a130cdc90ce35a0b
David Paul Jansen <david.jansen1@rwth-aachen.de>
2025-01-06 12:08:36 +0100

    fix handling of result dataframes for AggregatedThermalZone

--- bim2sim/plugins/PluginTEASER/bim2sim_teaser/task/create_result_df.py
+++ bim2sim/plugins/PluginTEASER/bim2sim_teaser/task/create_result_df.py
@@ -190,14 +190,13 @@ class CreateResultDF(ITask):
         """
         bim2sim_teaser_mapping = {}
         space_guid_list = []
+        all_tzs = filter_elements(elements, 'ThermalZone')
         agg_tzs = filter_elements(elements, 'AggregatedThermalZone')
-        if agg_tzs:
-            for agg_tz in agg_tzs:
-                space_guid_list.append(agg_tz.guid)
-        else:
-            tzs = filter_elements(elements, 'ThermalZone')
-            for tz in tzs:
-                space_guid_list.append(tz.guid)
+        normal_tzs = list(set(all_tzs) - set(agg_tzs))
+        for agg_tz in agg_tzs:
+            space_guid_list.append(agg_tz.guid)
+        for tz in normal_tzs:
+            space_guid_list.append(tz.guid)
         for key, value in bim2sim_teaser_mapping_selected.items():
             # add entry for each room/zone
             if "numZones" in key:
