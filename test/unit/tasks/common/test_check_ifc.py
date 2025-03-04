db09622feba1ad68573f3eff49a2c5f618bb8738
Your Name <falk.cudok@tu-berlin.de>
2025-03-04 14:25:49 +0100

    first version of ids test

--- test/unit/tasks/common/test_check_ifc.py
+++ test/unit/tasks/common/test_check_ifc.py
@@ -4,6 +4,11 @@ Based on ifctester and IDS (Information Delivery Specification)
 """
 
 import unittest
+import bim2sim.tasks.common.check_ifc_ids as check_ifc_ids
+
+# the paths must adapt to be generic
+ifc_file = '/home/cudok/Documents/10_Git/bim2sim/test/resources/arch/ifc/AC20-FZK-Haus_with_SB55.ifc'
+ids_file = '/home/cudok/Documents/12_ifc_check_ids/ifc_check_spaces(3).ids'
 
 class TestCheckIFC(unittest.TestCase):
 
@@ -14,6 +19,6 @@ class TestCheckIFC(unittest.TestCase):
         see:
         https://github.com/buildingSMART/IDS/tree/development/Documentation/ImplementersDocumentation/TestCases/ids
         """
-
+        all_checks_passed = check_ifc_ids.run_ids_check_on_ifc(ifc_file, ids_file)
         # dummy code, delete later
-        self.assertEqual(sum([1, 2, 3]), 8, "Should be 6")
+        self.assertEqual(all_checks_passed, False, "Should be false")
