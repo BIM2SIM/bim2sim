4ae3a526dfea44d4fb7c6a5de5b4361308c3a007
Your Name <falk.cudok@tu-berlin.de>
2025-03-11 11:43:08 +0100

    add/adapt tests check_ifc via ids

--- test/unit/tasks/common/test_check_ifc.py
+++ test/unit/tasks/common/test_check_ifc.py
@@ -12,7 +12,7 @@ test_rsrc_path = Path(__file__).parent.parent.parent.parent / 'resources'
 
 class TestCheckIFC(unittest.TestCase):
 
-    def test_checkIFC_IDS_fail(self):
+    def test_checkIFC_IDS_examples_minimal_fail(self):
         """check ifctester is working correctly by do check with IDS and ifc
         files from the IDS repo
 
@@ -20,8 +20,49 @@ class TestCheckIFC(unittest.TestCase):
         https://github.com/buildingSMART/IDS/tree/development/Documentation/ImplementersDocumentation/TestCases/ids
         """
 
-        ifc_file = test_rsrc_path / 'arch/ids/fail-a_minimal_ids_can_check_a_minimal_ifc_1_2.ifc'
-        ids_file = test_rsrc_path / 'arch/ids/fail-a_minimal_ids_can_check_a_minimal_ifc_1_2.ids'
+        ifc_file = test_rsrc_path / 'ids/fail-a_minimal_ids_can_check_a_minimal_ifc_1_2.ifc'
+        ids_file = test_rsrc_path / 'ids/fail-a_minimal_ids_can_check_a_minimal_ifc_1_2.ids'
+        all_checks_passed = check_ifc_ids.run_ids_check_on_ifc(ifc_file, ids_file)
+        self.assertEqual(all_checks_passed, False, "Should be false")
+
+    def test_checkIFC_IDS_examples_required_specifications_fail(self):
+        """check ifctester is working correctly by do check with IDS and ifc
+        files from the IDS repo
+
+        see:
+        https://github.com/buildingSMART/IDS/tree/development/Documentation/ImplementersDocumentation/TestCases/ids
+        """
+
+        ifc_file = test_rsrc_path / 'ids/fail-required_specifications_need_at_least_one_applicable_entity_2_2.ifc'
+        ids_file = test_rsrc_path / 'ids/fail-required_specifications_need_at_least_one_applicable_entity_2_2.ids'
+        all_checks_passed = check_ifc_ids.run_ids_check_on_ifc(ifc_file, ids_file)
+        self.assertEqual(all_checks_passed, False, "Should be false")
+
+    def test_checkIFC_IDS_examples_a_specification_pass(self):
+        """check ifctester is working correctly by do check with IDS and ifc
+        files from the IDS repo
+
+        see:
+        https://github.com/buildingSMART/IDS/tree/development/Documentation/ImplementersDocumentation/TestCases/ids
+        """
+
+        ifc_file = test_rsrc_path / 'ids/pass-a_specification_passes_only_if_all_requirements_pass_2_2.ifc'
+        ids_file = test_rsrc_path / 'ids/pass-a_specification_passes_only_if_all_requirements_pass_2_2.ids'
         all_checks_passed = check_ifc_ids.run_ids_check_on_ifc(ifc_file, ids_file)
         # dummy code, delete later
-        self.assertEqual(all_checks_passed, False, "Should be false")
+        self.assertEqual(all_checks_passed, True, "Should be true")
+
+
+    def test_checkIFC_IDS_examples_specification_optionality_pass(self):
+        """check ifctester is working correctly by do check with IDS and ifc
+        files from the IDS repo
+
+        see:
+        https://github.com/buildingSMART/IDS/tree/development/Documentation/ImplementersDocumentation/TestCases/ids
+        """
+
+        ifc_file = test_rsrc_path / 'ids/pass-specification_optionality_and_facet_optionality_can_be_combined.ifc'
+        ids_file = test_rsrc_path / 'ids/pass-specification_optionality_and_facet_optionality_can_be_combined.ids'
+        all_checks_passed = check_ifc_ids.run_ids_check_on_ifc(ifc_file, ids_file)
+        # dummy code, delete later
+        self.assertEqual(all_checks_passed, True, "Should be true")
