b609a1072ff581de513f5ca04c3e137b990f666d
Your Name <falk.cudok@tu-berlin.de>
2025-03-26 08:44:55 +0100

    uncomment guid Non-check and "log" print

--- bim2sim/tasks/common/check_ifc_ids.py
+++ bim2sim/tasks/common/check_ifc_ids.py
@@ -8,7 +8,7 @@ import webbrowser
 
 
 
-def run_check_guid(ifc_file: str) -> (bool, dict, dict):
+def run_check_guid_unique(ifc_file: str) -> (bool, dict):
     """check the uniqueness of the guids of the IFC file
 
     Input:
@@ -18,38 +18,46 @@ def run_check_guid(ifc_file: str) -> (bool, dict, dict):
         all_guids_unique: boolean
                       (true: all guids are unique
                        false: one or more guids are not unique)
+
        double_guid: dict
-       guids_none: dict
+
     """
     model = ifcopenshell.open(ifc_file)
 
     used_guids: dict[str, ifcopenshell.entity_instance] = dict() # dict of all elements with guids used in the checked ifc model
     double_guids: dict[str, ifcopenshell.entity_instance] = dict() # dict of elements with guids, which are not unique
     all_guids_unique = True
+
     # TODO entries of the guid_none list should include an attribute value, which helps to identify the element
-    guids_none : dict[str, ifcopenshell.entity_instance] = dict() # dict of elements, which have a guid value = None
-    guid_none_no = 0
+    # guids_none : dict[str, ifcopenshell.entity_instance] = dict() # dict of elements, which have a guid value = None
+    # guid_none_no = 0
+    #
     for inst in model:
        if hasattr(inst, "GlobalId"):
            guid = inst.GlobalId
            name = inst.Name
            # print(guid)
            if guid in used_guids:
-               print(guid)
                double_guids[guid] = inst
                all_guids_unique = False
-           elif guid in ['', 'None']:
-               # TODO if possible to check "guid = None/''" delete this region
-               # and adapt whole function
-               all_guids_unique = False
-               guid_none_no = guid_none_no + 1
-               name_dict = name + '--' + str(guid_none_no)
-               print("none: {}".format(name))
+           # elif guid in ['', 'None']:
+           #     # TODO if possible to check "guid = None/''" delete this region
+           #     # and adapt whole function
+           #     all_guids_unique = False
+           #     guid_none_no = guid_none_no + 1
+           #     name_dict = name + '--' + str(guid_none_no)
+           #     print("none: {}".format(name))
 
-               guids_none[name_dict] = inst
+           #     guids_none[name_dict] = inst
            else:
                used_guids[guid] = inst
-    return (all_guids_unique, double_guids, guids_none)
+
+    list_guids_non_unique = list(double_guids.keys())
+    print(">>>>>> ")
+    print("the GUIDs of all elements are unique: {}".format(all_guids_unique))
+    if all_guids_unique is False:
+        print("non-unique GUIDs: {}".format(list_guids_non_unique))
+    return (all_guids_unique, double_guids)
 
 
 def run_ids_check_on_ifc(ifc_file: str, ids_file: str, report_html: bool = False) -> bool:
@@ -99,4 +107,4 @@ if __name__ == '__main__':
     ifc_file_copy = '/home/cudok/Documents/12_ifc_check_ids/AC20-FZK-Haus_with_SB55_copyGUID.ifc'
     ifc_file_guid_error = '/home/cudok/Documents/12_ifc_check_ids/AC20-FZK-Haus_with_SB55_NoneAndDoubleGUID.ifc'
     # ids_file = '/home/cudok/Documents/12_ifc_check_ids/ifc_check_spaces(3).ids'
-    guid_check_passed, double_guid = run_check_guid(ifc_file_guid_error)
+    guid_check_passed, double_guid = run_check_guid_unique(ifc_file_guid_error)
