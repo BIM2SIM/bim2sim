4f54c948df58cc6f95242196caedc2188495e7ee
Your Name <falk.cudok@tu-berlin.de>
2025-06-11 16:11:37 +0200

    rename CheckIfcNew > CheckIfc and some adaptions

--- bim2sim/tasks/common/__init__.py
+++ bim2sim/tasks/common/__init__.py
@@ -2,7 +2,7 @@ from .create_relations import CreateRelations
 from .base_tasks import Reset, Quit
 from .load_ifc import LoadIFC
 from .check_ifc import CheckIfc, CheckIfcBPS, CheckIfcHVAC
-from .check_ifc_ids import CheckIfcNew
+from .check_ifc_ids import CheckIfc
 from .create_elements import CreateElementsOnIfcTypes
 from .weather import Weather
 from .serialize_elements import SerializeElements
