3722c318b9dc681b589ffd6ba6ccd5130f53365e
Your Name <falk.cudok@tu-berlin.de>
2025-06-17 16:24:21 +0200

    move test_checkIFC_guid_unique_specific_guid_return

--- test/unit/tasks/common/test_check_ifc.py
+++ test/unit/tasks/common/test_check_ifc.py
@@ -101,6 +101,33 @@ class TestCheckIFC(unittest.TestCase):
             all_guids_checks_passed, non_unique_guids = CheckIfc.run_check_guid_unique(self, ifc_file)
             self.assertEqual(all_guids_checks_passed, False, "Should be False")
 
+    def test_checkIFC_guid_unique_specific_guid_return(self):
+        """test the guid return of a failed GUID uniqueness check
+           the following represent a project using the DummyPlugin
+        """
+        # TODO move test ifc file into resources and adapt path
+        self.test_dir = tempfile.TemporaryDirectory()
+        ifc_paths = {
+            IFCDomain.arch: self.ifc_file_fkz_DoubleAndNoneGUID,
+        }
+        self.project = Project.create(self.test_dir.name, ifc_paths,
+                                 plugin=PluginDummy, )
+        # weather data path is mandatory and "mocking" is not working
+        # so use a central defintion of weather file
+        self.project.sim_settings.weather_file_path = self.weather_file_path()
+        # put project.run into DebugDecisionHandler is need, otherwise the
+        # playground.state() is empty and ifc_files are not available
+        handler = DebugDecisionHandler([ProductBased.key])
+        handler.handle(self.project.run(cleanup=False))
+
+        ifc_files = self.project.playground.state['ifc_files']
+
+        predicted_result = ['25OWQvmXj5BPgyergP43tY', '1Oms875aH3Wg$9l65H2ZGw']
+        for ifc_file in ifc_files:
+            # self.run_check_guid_unique(ifc_file)
+            all_guids_checks_passed, non_unique_guids = CheckIfc.run_check_guid_unique(self, ifc_file)
+            list_guids_non_unique = list(non_unique_guids.keys())
+            self.assertEqual(list_guids_non_unique, predicted_result, "Should be a list of 2 GUIDs")
 
 class TestCheckIFCSelfMade(unittest.TestCase):
     """Tests for function checking IFC files, which are self made (these needed
@@ -114,33 +141,6 @@ class TestCheckIFCSelfMade(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         print("run setUpClass")
-
-    # def test_checkIFC_guid_unique_pass(self):
-    #     """test the boolean of the GUID uniqueness check, check pass
-    #     """
-    #     # TODO move test ifc file into resources and adapt path
-    #     ifc_file_guid_ok = '/home/cudok/Documents/12_ifc_check_ids/AC20-FZK-Haus_with_SB55.ifc'
-    #     all_guids_checks_passed, non_unique_guids = check_ifc_ids.run_check_guid_unique(ifc_file_guid_ok)
-    #     self.assertEqual(all_guids_checks_passed, True, "Should be True")
-
-    def test_checkIFC_guid_unique_fail(self):
-        """test the boolean of the GUID uniqueness check, check fails
-        """
-        # TODO move test ifc file into resources and adapt path
-        ifc_file_guid_error = '/home/cudok/Documents/12_ifc_check_ids/AC20-FZK-Haus_with_SB55_NoneAndDoubleGUID.ifc'
-        all_guids_checks_passed, non_unique_guids = check_ifc_ids.run_check_guid_unique(ifc_file_guid_error)
-        self.assertEqual(all_guids_checks_passed, False, "Should be false")
-
-    def test_checkIFC_guid_unique_specifc_guid_return(self):
-        """test the guid return of a failed GUID uniqueness check
-        """
-        # TODO move test ifc file into resources and adapt path
-        ifc_file_guid_error = '/home/cudok/Documents/12_ifc_check_ids/AC20-FZK-Haus_with_SB55_NoneAndDoubleGUID.ifc'
-        all_guids_checks_passed, non_unique_guids = check_ifc_ids.run_check_guid_unique(ifc_file_guid_error)
-        list_guids_non_unique = list(non_unique_guids.keys())
-
-        predicted_result = ['25OWQvmXj5BPgyergP43tY', '1Oms875aH3Wg$9l65H2ZGw']
-        self.assertEqual(list_guids_non_unique, predicted_result, "Should be a list of 2 GUIDs")
 
     def test_run_check_guid_empty_fail(self):
         """test the boolean of all GUIDs has a value check, check fails
