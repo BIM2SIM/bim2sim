44de6f4a24485f92b989af3d775d4a15003c8a21
Your Name <falk.cudok@tu-berlin.de>
2025-07-01 12:09:48 +0200

    add tests with 3rooms ifc files
    delete not needed comments

--- bim2sim/plugins/PluginIFCCheck/test/integration/test_ifccheck.py
+++ bim2sim/plugins/PluginIFCCheck/test/integration/test_ifccheck.py
@@ -37,8 +37,6 @@ class TestIntegrationIFCCheck(IntegrationBaseIFCCheck, unittest.TestCase):
 
         project.sim_settings.setpoints_from_template = True
 
-        # run_project(project, ConsoleDecisionHandler())
-
         answers = ()
         handler = DebugDecisionHandler(answers)
         for decision, answer in handler.decision_answer_mapping(project.run()):
@@ -69,7 +67,75 @@ class TestIntegrationIFCCheck(IntegrationBaseIFCCheck, unittest.TestCase):
 
         project.sim_settings.setpoints_from_template = True
 
-        # run_project(project, ConsoleDecisionHandler())
+        answers = ('Other', 'Other')
+        handler = DebugDecisionHandler(answers)
+        for decision, answer in handler.decision_answer_mapping(project.run()):
+            decision.value = answer
+        self.assertEqual(0, handler.return_value,
+                         "Project did not finish successfully.")
+
+    def test_run_three_rooms_with_SB(self):
+        """Run project with
+        2024-04-23_3rooms_240317_Heater_AirTerminal_Table_with_SB.ifc"""
+
+        ifc_3rooms_with_sb = Path(
+            '/home/cudok/Documents/12_ifc_check_ids/03_ifc_veronika/2024-04-23_3rooms_240317_Heater_AirTerminal_Table_with_SB.ifc'
+        )
+        ifc_names = {IFCDomain.arch: ifc_3rooms_with_sb}
+        project = self.create_project(ifc_names, PluginIFCCheck)
+
+        # assign an IDS file, which is needed to check the ifc file by ifctester
+        # TODO exchange to a more sophisticated ids file
+        project.sim_settings.ids_file_path = (
+                Path(bim2sim.__file__).parent.parent /
+                'test/resources/ids/fail-a_minimal_ids_can_check_a_minimal_ifc_1_2.ids'
+        )
+
+        # In the next step we assign this file to the project by setting:
+        project.sim_settings.prj_custom_usages = (Path(
+            bim2sim.__file__).parent.parent / "test/resources/arch/custom_usages/"
+                "customUsagesAC20-FZK-Haus.json")
+
+        project.sim_settings.prj_use_conditions = (Path(
+            bim2sim.__file__).parent.parent / "test/resources/arch/custom_usages/"
+                "UseConditionsAC20-FZK-Haus.json")
+
+        project.sim_settings.setpoints_from_template = True
+
+        answers = ('Other', 'Other')
+        handler = DebugDecisionHandler(answers)
+        for decision, answer in handler.decision_answer_mapping(project.run()):
+            decision.value = answer
+        self.assertEqual(0, handler.return_value,
+                         "Project did not finish successfully.")
+
+    def test_run_three_rooms_with_SB_fail(self):
+        """Run project with
+        2024-04-23_3rooms_240317_Heater_AirTerminal_Table_with_SB.ifc"""
+
+        ifc_3rooms_with_sb_fail = Path(
+            '/home/cudok/Documents/12_ifc_check_ids/03_ifc_veronika/2024-04-23_3rooms_240317_Heater_AirTerminal_Table_with_SB_errors.ifc'
+        )
+        ifc_names = {IFCDomain.arch: ifc_3rooms_with_sb_fail}
+        project = self.create_project(ifc_names, PluginIFCCheck)
+
+        # assign an IDS file, which is needed to check the ifc file by ifctester
+        # TODO exchange to a more sophisticated ids file
+        project.sim_settings.ids_file_path = (
+                Path(bim2sim.__file__).parent.parent /
+                'test/resources/ids/fail-a_minimal_ids_can_check_a_minimal_ifc_1_2.ids'
+        )
+
+        # In the next step we assign this file to the project by setting:
+        project.sim_settings.prj_custom_usages = (Path(
+            bim2sim.__file__).parent.parent / "test/resources/arch/custom_usages/"
+                "customUsagesAC20-FZK-Haus.json")
+
+        project.sim_settings.prj_use_conditions = (Path(
+            bim2sim.__file__).parent.parent / "test/resources/arch/custom_usages/"
+                "UseConditionsAC20-FZK-Haus.json")
+
+        project.sim_settings.setpoints_from_template = True
 
         answers = ('Other', 'Other')
         handler = DebugDecisionHandler(answers)
