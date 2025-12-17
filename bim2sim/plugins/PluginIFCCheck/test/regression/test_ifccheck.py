d503084a9b8f5d058499f722a8df6e857aaba808
Your Name <falk.cudok@tu-berlin.de>
2025-12-17 17:06:36 +0100

    remove not needed part
    include maybe later

--- bim2sim/plugins/PluginIFCCheck/test/regression/test_ifccheck.py
+++ bim2sim/plugins/PluginIFCCheck/test/regression/test_ifccheck.py
@@ -16,41 +16,18 @@ from bim2sim.utilities.types import IFCDomain, ZoningCriteria
 logger = logging.getLogger(__name__)
 
 
-class RegressionTestTEASER(RegressionTestBase):
+class RegressionTestIFCCheck(RegressionTestBase):
+    """Class to setup up nad run regression test of PluginIFCCheck"""
     def setUp(self):
         self.results_src_dir = None
         self.results_dst_dir = None
-        self.tester = None
         super().setUp()
 
     def tearDown(self):
-        # clean up buildingspy logs
-        # TODO if statement is only needed, because CI discovers more tests
-        #  than existing and then fails when trying to access not existing
-        #  project
-        if self.project:
-            reg_dir = self.project.paths.b2sroot / 'bim2sim' / 'plugins' \
-                      / 'PluginIFCCheck' / 'test' / 'regression'
-            shutil.rmtree(reg_dir / 'funnel_comp', ignore_errors=True)
-            log_files = [
-                'comparison-dymola.log',
-                'failed-simulator-dymola.log',
-                'simulator-dymola.log',
-                'unitTests-dymola.log'
-            ]
-            for log_file in log_files:
-                source = reg_dir / log_file
-                if not self.is_ci:
-                    destination = self.project.paths.log / log_file
-                    if source.exists():
-                        shutil.move(str(source), str(destination))
 
         super().tearDown()
 
-    def create_regression_setup(
-            self,
-            tolerance: float = 1E-3,
-            batch_mode: bool = False):
+    def create_regression_setup(self):
         """
         Create a regression test setup based on BuildingsPy regression tests.
 
@@ -64,39 +41,12 @@ class RegressionTestTEASER(RegressionTestBase):
                 can be created
 
         """
-        regex = re.compile("[^a-zA-z0-9]")
-        model_export_name = regex.sub("", self.project.name)
-        self.ref_results_src_path = Path(bim2sim.__file__).parent.parent \
-            / "test/resources/arch/regression_results" \
-            / self.project.name / 'TEASER'
-        self.ref_results_dst_path = \
-            self.project.paths.export / 'TEASER' / 'Model' / \
-            model_export_name / 'Resources' / 'ReferenceResults' / \
-            'Dymola'
-        self.tester = u.Tester(tool='dymola', tol=tolerance,
-                               cleanup=True)
-        self.tester.pedanticModelica(False)
-        self.tester.showGUI(False)
-        self.tester.batchMode(batch_mode)
-        self.tester.setLibraryRoot(
-            self.project.paths.export / 'TEASER' / 'Model' / model_export_name)
-        path_aixlib = (
-                Path(bim2sim.__file__).parent / 'plugins' /
-                f'PluginTEASER' / 'test' / 'regression' / 'library' /
-                'library_AixLib' / 'AixLib' / 'package.mo')
-        self.tester.setAdditionalLibResource(str(path_aixlib))
-        if list(self.ref_results_src_path.rglob("*.txt")):
-            shutil.copytree(self.ref_results_src_path,
-                            self.ref_results_dst_path)
-        else:
-            shutil.rmtree(self.ref_results_src_path.parent, ignore_errors=True)
+        passed_regression_test = False
+        return passed_regression_test
 
     def run_regression_test(self):
-        if not list(self.ref_results_src_path.rglob("*.txt")):
-            logger.error(
-                f"No Regression Results found in {self.ref_results_src_path} "
-                f"to perform regression test via simulation.")
-        reg_test_res = self.tester.run()
+        print("run reg test")
+        reg_test_res = False
         return reg_test_res
 
     def create_regression_results(self):
@@ -109,29 +59,10 @@ class RegressionTestTEASER(RegressionTestBase):
         regression results nothing happens.
         If simulation is not successful nothing happens.
         """
-        self.tester.run()
-        sim_sucessful = self.tester._comp_info[0]['simulation']['success']
-        comp_sucessful = self.tester._comp_info[0]['comparison']['test_passed']
+        # TODO put something in
+        pass
 
-        # sim and comparison
-        if sim_sucessful and all(comp_sucessful):
-            logger.info("No differences between simulation and regression "
-                        "results. No fresh results created.")
-        # sim successful but comparison not (new results were created based if
-        # user decided to simulate
-        elif sim_sucessful and not all(comp_sucessful):
-            # copy updated ref results to assets
-            shutil.rmtree(self.ref_results_src_path,
-                          ignore_errors=True)
-            shutil.copytree(self.ref_results_dst_path,
-                            self.ref_results_src_path)
-            logger.info("Regression results were updated with new results.")
-        elif not sim_sucessful:
-            logger.error(f"The simulation was not successful, "
-                         f"no new regression results were created.")
-
-
-class TestRegressionTEASER(RegressionTestTEASER, unittest.TestCase):
+class TestRegressionIFCCheck(RegressionTestIFCCheck, unittest.TestCase):
     def test_run_kitfzkhaus(self):
         """Run TEASER regression test with AC20-FZK-Haus.ifc and one zone model
          export"""
@@ -160,5 +91,3 @@ class TestRegressionTEASER(RegressionTestTEASER, unittest.TestCase):
         self.assertEqual(0, reg_test_res,
                          "Regression test with simulation did not finish"
                          " successfully or created deviations.")
-
-print("DAS ist eine TEst")
