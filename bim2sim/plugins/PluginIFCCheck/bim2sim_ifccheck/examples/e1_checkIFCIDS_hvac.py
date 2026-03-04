ade1edf6e0d388e3946bb0840ab249f7a3258fb8
Your Name <falk.cudok@tu-berlin.de>
2026-03-04 08:17:59 +0100

    remove unnecessary imports and formal corrections

--- bim2sim/plugins/PluginIFCCheck/bim2sim_ifccheck/examples/e1_checkIFCIDS_hvac.py
+++ bim2sim/plugins/PluginIFCCheck/bim2sim_ifccheck/examples/e1_checkIFCIDS_hvac.py
@@ -1,19 +1,15 @@
+"""Simple example for PluginIFCCheck with hvac focus."""
 import tempfile
 from pathlib import Path
 
 import bim2sim
 from bim2sim import Project, ConsoleDecisionHandler, run_project
-from bim2sim.kernel.decision.decisionhandler import DebugDecisionHandler
-from bim2sim.elements import bps_elements
-from bim2sim.utilities.common_functions import filter_elements
 from bim2sim.utilities.types import IFCDomain
-from bim2sim.elements.base_elements import Material
 from bim2sim.plugins.PluginIFCCheck.bim2sim_ifccheck import PluginIFCCheck
 
 
 def run_simple_project():
-    """Run a bim2sim project with the PluginIFCCheck
-    """
+    """Run a bim2sim project with the PluginIFCCheck."""
     # Create a temp directory for the project, feel free to use a "normal"
     # directory
     project_path = Path(tempfile.TemporaryDirectory(
@@ -50,7 +46,6 @@ def run_simple_project():
             Path(bim2sim.__file__).parent /
             'plugins/PluginIFCCheck/bim2sim_ifccheck/ifc_hvac.ids'
     )
-
 
     # run the project
     run_project(project, ConsoleDecisionHandler())
